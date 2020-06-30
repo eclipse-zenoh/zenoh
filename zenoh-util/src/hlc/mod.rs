de5b8e1b01f231a921d956dd7e1fa192ef71223e
Julien Enoch <julien.enoch@adlinktech.com>
2020-06-30 19:31:04 +0200

    Make HLC async

--- zenoh-util/src/hlc/mod.rs
+++ zenoh-util/src/hlc/mod.rs
@@ -14,6 +14,7 @@
 use std::cmp;
 use std::time::{SystemTime, Duration, UNIX_EPOCH};
 use log::warn;
+use async_std::sync::Mutex;
 
 mod ntp64;
 pub use ntp64::*;
@@ -35,28 +36,29 @@ const DELTA_MS: u64 = 100;
 pub struct HLC {
     id: Vec<u8>,
     delta: NTP64,
-    last_time: NTP64
+    last_time: Mutex<NTP64>
 }
 
 impl HLC {
 
     pub fn new(id: Vec<u8>) -> HLC {
         let delta = NTP64::from(Duration::from_millis(DELTA_MS));
-        HLC { id, delta, last_time: NTP64::from(UNIX_EPOCH) }
+        HLC { id, delta, last_time: Mutex::new(NTP64::from(UNIX_EPOCH)) }
     }
 
-    pub fn new_timestamp(&mut self) -> Timestamp {
+    pub async fn new_timestamp(&self) -> Timestamp {
         let mut now = NTP64::from(SystemTime::now());
         now.0 &= LMASK;
-        if now.0 > (self.last_time.0 & LMASK) {
-            self.last_time = now
+        let mut last_time = self.last_time.lock().await;
+        if now.0 > (last_time.0 & LMASK) {
+            *last_time = now
         } else {
-            self.last_time += 1;
+            *last_time += 1;
         }
-        Timestamp::new(self.last_time, self.id.clone())
+        Timestamp::new(*last_time, self.id.clone())
     }
 
-    pub fn update_with_timestamp(&mut self, timestamp: Timestamp) -> Result<(), String> {
+    pub async fn update_with_timestamp(&self, timestamp: Timestamp) -> Result<(), String> {
         let mut now = NTP64::from(SystemTime::now());
         now.0 &= LMASK;
         let msg_time = timestamp.get_time();
@@ -68,13 +70,14 @@ impl HLC {
             warn!("{}", err_msg);
             Err(err_msg)
         } else {
-            let max_time = cmp::max(cmp::max(now, *msg_time), self.last_time);
+            let mut last_time = self.last_time.lock().await;
+            let max_time = cmp::max(cmp::max(now, *msg_time), *last_time);
             if max_time == now {
-                self.last_time = now;
+                *last_time = now;
             } else if max_time == *msg_time {
-                self.last_time = *msg_time + 1; 
+                *last_time = *msg_time + 1; 
             } else {
-                self.last_time += 1;
+                *last_time += 1;
             }
             Ok(())
         }
@@ -83,41 +86,45 @@ impl HLC {
 
 
 
-
 #[cfg(test)]
 mod tests {
     use crate::hlc::*;
     use std::time::{Duration, UNIX_EPOCH};
+    use async_std::task;
 
     #[test]
     fn test_hlc() {
-        let id: Vec<u8> = vec![
-            0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 
-            0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f, 0x10];
-        let mut hlc = HLC::new(id.clone());
-        
-        // Test that 10000 generated timestamps in a few time are monotonics
-        let times: Vec<Timestamp> = 
-            std::iter::repeat(()).take(10000).map(|()| hlc.new_timestamp()).collect();
-        let mut iter = times.iter().peekable();
-        while let Some(ts1) = iter.next() {
-            if let Some(ts2) = iter.peek() {
-                assert!(ts1 < ts2);
-                assert_eq!(ts1.get_id(), ts2.get_id());
+        task::block_on(async{
+            let id: Vec<u8> = vec![
+                0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 
+                0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f, 0x10];
+            let hlc = HLC::new(id.clone());
+            
+            // Test that 10000 generated timestamps in a few time are monotonics
+            let mut times: Vec<Timestamp> = Vec::with_capacity(10000);
+            for _ in 0..10000 {
+                times.push(hlc.new_timestamp().await)
             }
-        }
 
-        // Test that updating with an old Timestamp don't break the HLC
-        let past_ts = Timestamp::new(NTP64::from(UNIX_EPOCH), id.clone());
-        let now_ts = hlc.new_timestamp();
-        assert!(hlc.update_with_timestamp(past_ts).is_ok());
-        assert!(hlc.new_timestamp() > now_ts);
+            let mut iter = times.iter().peekable();
+            while let Some(ts1) = iter.next() {
+                if let Some(ts2) = iter.peek() {
+                    assert!(ts1 < ts2);
+                    assert_eq!(ts1.get_id(), ts2.get_id());
+                }
+            }
 
-        // Test that updating with a Timestamp exceeding the delta is refused
-        let now_ts = hlc.new_timestamp();
-        let future_time = now_ts.get_time() + NTP64::from(Duration::from_millis(500));
-        let future_ts = Timestamp::new(future_time , id.clone());
-        assert!(hlc.update_with_timestamp(future_ts).is_err())
+            // Test that updating with an old Timestamp don't break the HLC
+            let past_ts = Timestamp::new(NTP64::from(UNIX_EPOCH), id.clone());
+            let now_ts = hlc.new_timestamp().await;
+            assert!(hlc.update_with_timestamp(past_ts).await.is_ok());
+            assert!(hlc.new_timestamp().await > now_ts);
 
+            // Test that updating with a Timestamp exceeding the delta is refused
+            let now_ts = hlc.new_timestamp().await;
+            let future_time = now_ts.get_time() + NTP64::from(Duration::from_millis(500));
+            let future_ts = Timestamp::new(future_time , id.clone());
+            assert!(hlc.update_with_timestamp(future_ts).await.is_err())
+        });
     }
 }
