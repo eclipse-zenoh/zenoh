5d1ebf8dd9f2751e6d5c126cc161f1c93e7b17fa
Julien Enoch <julien.enoch@adlinktech.com>
2020-08-25 12:36:28 +0200

    Add z_delete example

---
+++ zenoh/examples/zenoh/z_delete.rs
@@ -0,0 +1,82 @@
+//
+// Copyright (c) 2017, 2020 ADLINK Technology Inc.
+//
+// This program and the accompanying materials are made available under the
+// terms of the Eclipse Public License 2.0 which is available at
+// http://www.eclipse.org/legal/epl-2.0, or the Apache License, Version 2.0
+// which is available at https://www.apache.org/licenses/LICENSE-2.0.
+//
+// SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
+//
+// Contributors:
+//   ADLINK zenoh team, <zenoh@adlink-labs.tech>
+//
+use clap::{App, Arg};
+use std::convert::TryInto;
+use zenoh::net::Config;
+use zenoh::*;
+
+//
+// Argument parsing -- look at the main for the zenoh-related code
+//
+fn parse_args() -> (Config, String) {
+    let args = App::new("zenoh delete example")
+        .arg(
+            Arg::from_usage("-m, --mode=[MODE] 'The zenoh session mode.")
+                .possible_values(&["peer", "client"])
+                .default_value("peer"),
+        )
+        .arg(Arg::from_usage(
+            "-e, --peer=[LOCATOR]...  'Peer locators used to initiate the zenoh session.'",
+        ))
+        .arg(Arg::from_usage(
+            "-l, --listener=[LOCATOR]...   'Locators to listen on.'",
+        ))
+        .arg(
+            Arg::from_usage("-p, --path=[PATH]        'The name of the resource to delete.'")
+                .default_value("/demo/example/zenoh-rs-put"),
+        )
+        .get_matches();
+
+    let config = Config::default()
+        .mode(
+            args.value_of("mode")
+                .map(|m| Config::parse_mode(m))
+                .unwrap()
+                .unwrap(),
+        )
+        .add_peers(
+            args.values_of("peer")
+                .map(|p| p.collect())
+                .or_else(|| Some(vec![]))
+                .unwrap(),
+        )
+        .add_listeners(
+            args.values_of("listener")
+                .map(|p| p.collect())
+                .or_else(|| Some(vec![]))
+                .unwrap(),
+        );
+    let path = args.value_of("path").unwrap().to_string();
+
+    (config, path)
+}
+
+#[async_std::main]
+async fn main() {
+    // initiate logging
+    env_logger::init();
+
+    let (config, path) = parse_args();
+
+    println!("New zenoh...");
+    let zenoh = Zenoh::new(config, None).await.unwrap();
+
+    println!("New workspace...");
+    let workspace = zenoh.workspace(None).await.unwrap();
+
+    println!("Delete Path '{}'...\n", path);
+    workspace.delete(&path.try_into().unwrap()).await.unwrap();
+
+    zenoh.close().await.unwrap();
+}
