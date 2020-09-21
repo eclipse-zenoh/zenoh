f0bff73d824dc5fdecfab7cbe33696e3653b985a
Olivier Hécart <olivier.hecart@adlinktech.com>
2020-09-21 15:48:05 +0200

    Add readthedocs documentation

---
+++ zenoh/docs/conf.py
@@ -0,0 +1,29 @@
+#
+# Copyright (c) 2017, 2020 ADLINK Technology Inc.
+#
+# This program and the accompanying materials are made available under the
+# terms of the Eclipse Public License 2.0 which is available at
+# http://www.eclipse.org/legal/epl-2.0, or the Apache License, Version 2.0
+# which is available at https://www.apache.org/licenses/LICENSE-2.0.
+#
+# SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
+#
+# Contributors:
+#   ADLINK zenoh team, <zenoh@adlink-labs.tech>
+#
+
+# Configuration file for the Sphinx documentation builder.
+
+# -- Project information -----------------------------------------------------
+project = 'zenoh-rust'
+copyright = '2017, 2020 ADLINK Technology Inc'
+author = 'ADLINK zenoh team'
+release = '0.5.O-beta1'
+
+# -- General configuration ---------------------------------------------------
+master_doc = 'index'
+extensions = []
+language = 'rust'
+
+# -- Options for HTML output -------------------------------------------------
+html_theme = 'sphinx_rtd_theme'
