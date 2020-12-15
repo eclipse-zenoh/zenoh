c23686931d4fefb4cb9fb1d26314489345363dbd
Luca Cominardi <luca.cominardi@gmail.com>
2020-12-15 11:22:31 +0100

    Frame codec bench

--- zenoh-protocol/benches/data_creation.rs
+++ zenoh-protocol/benches/data_creation.rs
@@ -30,46 +30,66 @@ fn consume_message_arc(msg: Arc<ZenohMessage>) {
 }
 
 fn criterion_benchmark(c: &mut Criterion) {
-    let iters = [
-        1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 1, 8, 64, 128, 1024,
-    ];
-    let reliability = Reliability::Reliable;
-    let congestion_control = CongestionControl::Block;
-
-    for n in &iters {
-        let res_key = ResKey::RIdWithSuffix(18, String::from("/com/acme/sensors/temp"));
-        let info = Some(DataInfo {
-            source_id: Some(PeerId::new(16, [0u8; PeerId::MAX_SIZE])),
-            source_sn: Some(12345),
-            first_router_id: Some(PeerId::new(16, [0u8; PeerId::MAX_SIZE])),
-            first_router_sn: Some(12345),
-            timestamp: Some(uhlc::Timestamp::new(
-                Default::default(),
-                uhlc::ID::new(16, [1u8; uhlc::ID::MAX_SIZE]),
-            )),
-            kind: Some(0),
-            encoding: Some(0),
-        });
-        let payload = RBuf::from(vec![0; 1024]);
-
-        c.bench_function(format!("{} msg_creation", n).as_str(), |b| {
+    let size = [8, 16, 32, 64, 128, 256, 512, 1_024, 2_048, 4_096, 8_192];
+    for s in size.iter() {
+        c.bench_function(format!("{} msg_creation_yes_info", s).as_str(), |b| {
             b.iter(|| {
-                for _ in 0..*n {
-                    let msg = ZenohMessage::make_data(
-                        res_key.clone(),
-                        payload.clone(),
-                        reliability,
-                        congestion_control,
-                        info.clone(),
-                        None,
-                        None,
-                    );
-                    consume_message(msg);
-                }
+                let reliability = Reliability::Reliable;
+                let congestion_control = CongestionControl::Block;
+
+                let res_key = ResKey::RIdWithSuffix(18, String::from("/com/acme/sensors/temp"));
+                let info = Some(DataInfo {
+                    source_id: Some(PeerId::new(16, [0u8; PeerId::MAX_SIZE])),
+                    source_sn: Some(12345),
+                    first_router_id: Some(PeerId::new(16, [0u8; PeerId::MAX_SIZE])),
+                    first_router_sn: Some(12345),
+                    timestamp: Some(uhlc::Timestamp::new(
+                        Default::default(),
+                        uhlc::ID::new(16, [1u8; uhlc::ID::MAX_SIZE]),
+                    )),
+                    kind: Some(0),
+                    encoding: Some(0),
+                });
+                let payload = RBuf::from(vec![0; *s]);
+
+                let msg = ZenohMessage::make_data(
+                    res_key,
+                    payload,
+                    reliability,
+                    congestion_control,
+                    info,
+                    None,
+                    None,
+                );
+                consume_message(msg);
+            })
+        });
+
+        c.bench_function(format!("{} msg_creation_no_info", s).as_str(), |b| {
+            b.iter(|| {
+                let reliability = Reliability::Reliable;
+                let congestion_control = CongestionControl::Block;
+
+                let res_key = ResKey::RIdWithSuffix(18, String::from("/com/acme/sensors/temp"));
+                let info = None;
+                let payload = RBuf::from(vec![0; *s]);
+
+                let msg = ZenohMessage::make_data(
+                    res_key,
+                    payload,
+                    reliability,
+                    congestion_control,
+                    info,
+                    None,
+                    None,
+                );
+                consume_message(msg);
             })
         });
     }
 
+    let reliability = Reliability::Reliable;
+    let congestion_control = CongestionControl::Block;
     let res_key = ResKey::RIdWithSuffix(18, String::from("/com/acme/sensors/temp"));
     let info = Some(DataInfo {
         source_id: Some(PeerId::new(16, [0u8; PeerId::MAX_SIZE])),
@@ -94,16 +114,12 @@ fn criterion_benchmark(c: &mut Criterion) {
         None,
     ));
 
-    for n in &iters {
-        let amsg = msg.clone();
-        c.bench_function(format!("{} arc_msg_clone", n).as_str(), |b| {
-            b.iter(|| {
-                for _ in 0..*n {
-                    consume_message_arc(amsg.clone());
-                }
-            })
-        });
-    }
+    let amsg = msg.clone();
+    c.bench_function(format!("arc_msg_clone").as_str(), |b| {
+        b.iter(|| {
+            consume_message_arc(amsg.clone());
+        })
+    });
 }
 
 criterion_group!(benches, criterion_benchmark);
