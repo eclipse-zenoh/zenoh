a35e98949d143fa58c77df1bcbdd7239a9b53fa7
Luca Cominardi <luca.cominardi@gmail.com>
2020-12-16 13:05:38 +0100

    Improved ChannelLink consume future

--- zenoh-protocol/src/session/channel/link.rs
+++ zenoh-protocol/src/session/channel/link.rs
@@ -22,8 +22,7 @@ use async_std::task;
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::time::Duration;
 use zenoh_util::collections::{TimedEvent, TimedHandle, Timer};
-use zenoh_util::core::{ZError, ZErrorKind, ZResult};
-use zenoh_util::zerror;
+use zenoh_util::core::ZResult;
 
 // Consume task
 async fn consume_task(
@@ -31,42 +30,27 @@ async fn consume_task(
     link: Link,
     receiver: Receiver<()>,
 ) -> ZResult<()> {
-    enum Action {
-        Continue,
-        Stop,
-    }
-
-    async fn consume(queue: &Arc<TransmissionPipeline>, link: &Link) -> ZResult<Action> {
-        // Pull a serialized batch from the queue
-        let (batch, index) = queue.pull().await;
-        // Send the buffer on the link
-        link.send(batch.get_buffer()).await?;
-        // Reinsert the batch into the queue
-        queue.refill(batch, index).await;
-
-        Ok(Action::Continue)
-    }
-
-    async fn signal(receiver: &Receiver<()>) -> ZResult<Action> {
-        let res = receiver.recv().await;
-        match res {
-            Ok(_) => Ok(Action::Stop),
-            Err(_) => zerror!(ZErrorKind::Other {
-                descr: "Signal error".to_string()
-            }),
-        }
-    }
+    let mut result: ZResult<()> = Ok(());
 
     // Keep draining the queue
-    loop {
-        let action = consume(&queue, &link).race(signal(&receiver)).await?;
-        match action {
-            Action::Continue => continue,
-            Action::Stop => break,
+    let consume = async {
+        loop {
+            // Pull a serialized batch from the queue
+            let (batch, index) = queue.pull().await;
+            // Send the buffer on the link
+            result = link.send(batch.get_buffer()).await;
+            if result.is_err() {
+                break Ok(());
+            }
+            // Reinsert the batch into the queue
+            queue.refill(batch, index).await;
+            // Yield now to allow the queue to fill
+            task::yield_now().await;
         }
-    }
+    };
+    let _ = consume.race(receiver.recv()).await;
 
-    Ok(())
+    result
 }
 
 pub(crate) struct LinkAlive {
