eclipse-zenoh/zenoh

commit 32a5f3ccd5e3a0f92771ef7e53c7a1512b99852a

Improve context mapping in critical path

zenoh/src/net/routing/network.rs
@@ -39,7 +39,8 @@ impl std::fmt::Debug for Node {
 
 pub(crate) struct Link {
     pub(crate) session: Session,
-    pub(crate) mappings: VecMap<PeerId>,
+    mappings: VecMap<PeerId>,
+    local_mappings: VecMap<ZInt>,
 }
 
 impl Link {
@@ -47,6 +48,7 @@ impl Link {
         Link {
             session,
             mappings: VecMap::new(),
+            local_mappings: VecMap::new(),
         }
     }
 
@@ -58,6 +60,17 @@ impl Link {
     #[inline]
     pub(crate) fn get_pid(&self, psid: &ZInt) -> Option<&PeerId> {
         self.mappings.get((*psid).try_into().unwrap())
+    }
+
+    #[inline]
+    pub(crate) fn set_local_psid_mapping(&mut self, psid: ZInt, local_psid: ZInt) {
+        self.local_mappings
+            .insert(psid.try_into().unwrap(), local_psid);
+    }
+
+    #[inline]
+    pub(crate) fn get_local_psid(&self, psid: &ZInt) -> Option<&ZInt> {
+        self.local_mappings.get((*psid).try_into().unwrap())
     }
 }
 
@@ -116,22 +129,41 @@ impl Network {
         )
     }
 
+    #[inline]
     pub(crate) fn get_idx(&self, pid: &PeerId) -> Option<NodeIndex> {
         self.graph
             .node_indices()
             .find(|idx| self.graph[*idx].pid == *pid)
     }
 
-    pub(crate) fn get_link(&self, pid: &PeerId) -> Option<&Link> {
+    #[inline]
+    pub(crate) fn get_link(&self, id: usize) -> &Link {
+        &self.links[id]
+    }
+
+    #[inline]
+    pub(crate) fn get_link_from_pid(&self, pid: &PeerId) -> Option<&Link> {
         self.links
             .iter()
             .find(|link| link.session.get_pid().unwrap() == *pid)
     }
 
-    pub(crate) fn get_link_mut(&mut self, pid: &PeerId) -> Option<&mut Link> {
-        self.links
-            .iter_mut()
-            .find(|link| link.session.get_pid().unwrap() == *pid)
+    #[inline]
+    pub(crate) fn get_local_context(&self, context: ZInt, link_id: usize) -> usize {
+        (*self.get_link(link_id).get_local_psid(&context).unwrap())
+            .try_into()
+            .unwrap()
+    }
+
+    fn add_node(&mut self, node: Node) -> NodeIndex {
+        let pid = node.pid.clone();
+        let idx = self.graph.add_node(node);
+        for link in &mut self.links {
+            if let Some((psid, _)) = link.mappings.iter().find(|(_, p)| **p == pid) {
+                link.local_mappings.insert(psid, idx.index() as ZInt);
+            }
+        }
+        idx
     }
 
     async fn make_link_state(&self, idx: NodeIndex, details: bool) -> LinkState {
@@ -231,7 +263,13 @@ impl Network {
     ) -> Vec<(NodeIndex, Node)> {
         log::trace!("{} Received from {} raw: {:?}", self.name, src, link_states);
 
-        let src_link = match self.get_link_mut(&src) {
+        let graph = &self.graph;
+        let links = &mut self.links;
+
+        let src_link = match links
+            .iter_mut()
+            .find(|link| link.session.get_pid().unwrap() == src)
+        {
             Some(link) => link,
             None => {
                 log::error!(
@@ -249,6 +287,9 @@ impl Network {
             .filter_map(|link_state| {
                 if let Some(pid) = link_state.pid {
                     src_link.set_pid_mapping(link_state.psid, pid.clone());
+                    if let Some(idx) = graph.node_indices().find(|idx| graph[*idx].pid == pid) {
+                        src_link.set_local_psid_mapping(link_state.psid, idx.index() as u64);
+                    }
                     Some((
                         pid,
                         link_state.whatami.or(Some(whatami::ROUTER)).unwrap(),
@@ -279,7 +320,7 @@ impl Network {
             .collect::<Vec<(PeerId, whatami::Type, Option<Vec<Locator>>, ZInt, Vec<ZInt>)>>();
 
         // apply psid<->pid mapping to links
-        let src_link = self.get_link(&src).unwrap();
+        let src_link = self.get_link_from_pid(&src).unwrap();
         let link_states = link_states
             .into_iter()
             .map(|(pid, wai, locs, sn, links)| {
@@ -356,7 +397,7 @@ impl Network {
                             links: links.clone(),
                         };
                         log::debug!("{} Add node (state) {}", self.name, pid);
-                        let idx = self.graph.add_node(node);
+                        let idx = self.add_node(node);
                         Some((links, idx, true))
                     }
                 },
@@ -386,7 +427,7 @@ impl Network {
                         links: vec![],
                     };
                     log::debug!("{} Add node (reintroduced) {}", self.name, link.clone());
-                    let idx = self.graph.add_node(node);
+                    let idx = self.add_node(node);
                     reintroduced_nodes.push((vec![], idx, true));
                 }
             }
@@ -475,7 +516,7 @@ impl Network {
         removed
     }
 
-    pub(crate) async fn add_link(&mut self, session: Session) {
+    pub(crate) async fn add_link(&mut self, session: Session) -> usize {
         self.links.push(Link::new(session.clone()));
 
         let pid = session.get_pid().unwrap();
@@ -485,7 +526,7 @@ impl Network {
             None => {
                 log::debug!("{} Add node (link) {}", self.name, pid);
                 (
-                    self.graph.add_node(Node {
+                    self.add_node(Node {
                         pid: pid.clone(),
                         whatami,
                         locators: None,
@@ -517,6 +558,7 @@ impl Network {
 
         let idxs = self.graph.node_indices().map(|i| (i, true)).collect();
         self.send_on_link(idxs, &session).await;
+        self.links.len() - 1
     }
 
     pub(crate) async fn remove_link(&mut self, session: &Session) -> Vec<(NodeIndex, Node)> {

zenoh/src/net/routing/face.rs
@@ -28,6 +28,7 @@ pub struct FaceState {
     pub(super) pid: PeerId,
     pub(super) whatami: WhatAmI,
     pub(super) primitives: OutSession,
+    pub(super) link_id: usize,
     pub(super) local_mappings: HashMap<ZInt, Arc<Resource>>,
     pub(super) remote_mappings: HashMap<ZInt, Arc<Resource>>,
     pub(super) local_subs: Vec<Arc<Resource>>,
@@ -44,12 +45,14 @@ impl FaceState {
         pid: PeerId,
         whatami: WhatAmI,
         primitives: OutSession,
+        link_id: usize,
     ) -> Arc<FaceState> {
         Arc::new(FaceState {
             id,
             pid,
             whatami,
             primitives,
+            link_id,
             local_mappings: HashMap::new(),
             remote_mappings: HashMap::new(),
             local_subs: Vec::new(),
@@ -114,20 +117,15 @@ impl Face {
                         .routers_net
                         .as_ref()
                         .unwrap()
-                        .get_link(&self.state.pid)
+                        .get_link(self.state.link_id)
+                        .get_pid(&routing_context)
                     {
-                        Some(link) => match link.get_pid(&routing_context) {
-                            Some(router) => router.clone(),
-                            None => {
-                                log::error!(
-                                        "Received router subscription with unknown routing context id {}",
-                                        routing_context
-                                    );
-                                return;
-                            }
-                        },
+                        Some(router) => router.clone(),
                         None => {
-                            log::error!("Cannot find net context for face {}", self.state.pid);
+                            log::error!(
+                                "Received router subscription with unknown routing context id {}",
+                                routing_context
+                            );
                             return;
                         }
                     };
@@ -152,19 +150,19 @@ impl Face {
             | (whatami::PEER, whatami::ROUTER)
             | (whatami::PEER, whatami::PEER) => match routing_context {
                 Some(routing_context) => {
-                    let peer = match tables.peers_net.as_ref().unwrap().get_link(&self.state.pid) {
-                        Some(link) => match link.get_pid(&routing_context) {
-                            Some(peer) => peer.clone(),
-                            None => {
-                                log::error!(
-                                    "Received peer subscription with unknown routing context id {}",
-                                    routing_context
-                                );
-                                return;
-                            }
-                        },
+                    let peer = match tables
+                        .peers_net
+                        .as_ref()
+                        .unwrap()
+                        .get_link(self.state.link_id)
+                        .get_pid(&routing_context)
+                    {
+                        Some(peer) => peer.clone(),
                         None => {
-                            log::error!("Cannot find net context for face {}", self.state.pid);
+                            log::error!(
+                                "Received peer subscription with unknown routing context id {}",
+                                routing_context
+                            );
                             return;
                         }
                     };
@@ -212,20 +210,15 @@ impl Face {
                         .routers_net
                         .as_ref()
                         .unwrap()
-                        .get_link(&self.state.pid)
+                        .get_link(self.state.link_id)
+                        .get_pid(&routing_context)
                     {
-                        Some(link) => match link.get_pid(&routing_context) {
-                            Some(router) => router.clone(),
-                            None => {
-                                log::error!(
-                                        "Received router forget subscription with unknown routing context id {}",
-                                        routing_context
-                                    );
-                                return;
-                            }
-                        },
+                        Some(router) => router.clone(),
                         None => {
-                            log::error!("Cannot find net context for face {}", self.state.pid);
+                            log::error!(
+                                "Received router forget subscription with unknown routing context id {}",
+                                routing_context
+                            );
                             return;
                         }
                     };
@@ -249,19 +242,19 @@ impl Face {
             | (whatami::PEER, whatami::ROUTER)
             | (whatami::PEER, whatami::PEER) => match routing_context {
                 Some(routing_context) => {
-                    let peer = match tables.peers_net.as_ref().unwrap().get_link(&self.state.pid) {
-                        Some(link) => match link.get_pid(&routing_context) {
-                            Some(peer) => peer.clone(),
-                            None => {
-                                log::error!(
-                                    "Received peer forget subscription with unknown routing context id {}",
-                                    routing_context
-                                );
-                                return;
-                            }
-                        },
+                    let peer = match tables
+                        .peers_net
+                        .as_ref()
+                        .unwrap()
+                        .get_link(self.state.link_id)
+                        .get_pid(&routing_context)
+                    {
+                        Some(peer) => peer.clone(),
                         None => {
-                            log::error!("Cannot find net context for face {}", self.state.pid);
+                            log::error!(
+                                "Received peer forget subscription with unknown routing context id {}",
+                                routing_context
+                            );
                             return;
                         }
                     };
@@ -308,20 +301,15 @@ impl Face {
                         .routers_net
                         .as_ref()
                         .unwrap()
-                        .get_link(&self.state.pid)
+                        .get_link(self.state.link_id)
+                        .get_pid(&routing_context)
                     {
-                        Some(link) => match link.get_pid(&routing_context) {
-                            Some(router) => router.clone(),
-                            None => {
-                                log::error!(
-                                    "Received router queryable with unknown routing context id {}",
-                                    routing_context
-                                );
-                                return;
-                            }
-                        },
+                        Some(router) => router.clone(),
                         None => {
-                            log::error!("Cannot find net context for face {}", self.state.pid);
+                            log::error!(
+                                "Received router queryable with unknown routing context id {}",
+                                routing_context
+                            );
                             return;
                         }
                     };
@@ -345,19 +333,19 @@ impl Face {
             | (whatami::PEER, whatami::ROUTER)
             | (whatami::PEER, whatami::PEER) => match routing_context {
                 Some(routing_context) => {
-                    let peer = match tables.peers_net.as_ref().unwrap().get_link(&self.state.pid) {
-                        Some(link) => match link.get_pid(&routing_context) {
-                            Some(peer) => peer.clone(),
-                            None => {
-                                log::error!(
-                                    "Received peer queryable with unknown routing context id {}",
-                                    routing_context
-                                );
-                                return;
-                            }
-                        },
+                    let peer = match tables
+                        .peers_net
+                        .as_ref()
+                        .unwrap()
+                        .get_link(self.state.link_id)
+                        .get_pid(&routing_context)
+                    {
+                        Some(peer) => peer.clone(),
                         None => {
-                            log::error!("Cannot find net context for face {}", self.state.pid);
+                            log::error!(
+                                "Received peer queryable with unknown routing context id {}",
+                                routing_context
+                            );
                             return;
                         }
                     };
@@ -394,20 +382,15 @@ impl Face {
                         .routers_net
                         .as_ref()
                         .unwrap()
-                        .get_link(&self.state.pid)
+                        .get_link(self.state.link_id)
+                        .get_pid(&routing_context)
                     {
-                        Some(link) => match link.get_pid(&routing_context) {
-                            Some(router) => router.clone(),
-                            None => {
-                                log::error!(
-                                        "Received router forget queryable with unknown routing context id {}",
-                                        routing_context
-                                    );
-                                return;
-                            }
-                        },
+                        Some(router) => router.clone(),
                         None => {
-                            log::error!("Cannot find net context for face {}", self.state.pid);
+                            log::error!(
+                                "Received router forget queryable with unknown routing context id {}",
+                                routing_context
+                            );
                             return;
                         }
                     };
@@ -431,19 +414,19 @@ impl Face {
             | (whatami::PEER, whatami::ROUTER)
             | (whatami::PEER, whatami::PEER) => match routing_context {
                 Some(routing_context) => {
-                    let peer = match tables.peers_net.as_ref().unwrap().get_link(&self.state.pid) {
-                        Some(link) => match link.get_pid(&routing_context) {
-                            Some(peer) => peer.clone(),
-                            None => {
-                                log::error!(
-                                    "Received peer forget queryable with unknown routing context id {}",
-                                    routing_context
-                                );
-                                return;
-                            }
-                        },
+                    let peer = match tables
+                        .peers_net
+                        .as_ref()
+                        .unwrap()
+                        .get_link(self.state.link_id)
+                        .get_pid(&routing_context)
+                    {
+                        Some(peer) => peer.clone(),
                         None => {
-                            log::error!("Cannot find net context for face {}", self.state.pid);
+                            log::error!(
+                                "Received peer forget queryable with unknown routing context id {}",
+                                routing_context
+                            );
                             return;
                         }
                     };

zenoh/src/net/routing/pubsub.rs
@@ -954,16 +954,8 @@ pub async fn route_data(
                 whatami::ROUTER => match face.whatami {
                     whatami::ROUTER => {
                         let routers_net = tables.routers_net.as_ref().unwrap();
-                        let local_context = routers_net
-                            .get_idx(
-                                &routers_net
-                                    .get_link(&face.pid)
-                                    .unwrap()
-                                    .get_pid(&routing_context.unwrap())
-                                    .unwrap(),
-                            )
-                            .unwrap()
-                            .index();
+                        let local_context =
+                            routers_net.get_local_context(routing_context.unwrap(), face.link_id);
                         match &res {
                             Some(res) => res.routers_data_routes[local_context].clone(),
                             None => compute_data_route(
@@ -977,16 +969,8 @@ pub async fn route_data(
                     }
                     whatami::PEER => {
                         let peers_net = tables.peers_net.as_ref().unwrap();
-                        let local_context = peers_net
-                            .get_idx(
-                                &peers_net
-                                    .get_link(&face.pid)
-                                    .unwrap()
-                                    .get_pid(&routing_context.unwrap())
-                                    .unwrap(),
-                            )
-                            .unwrap()
-                            .index();
+                        let local_context =
+                            peers_net.get_local_context(routing_context.unwrap(), face.link_id);
                         match &res {
                             Some(res) => res.peers_data_routes[local_context].clone(),
                             None => compute_data_route(
@@ -1006,16 +990,8 @@ pub async fn route_data(
                 whatami::PEER => match face.whatami {
                     whatami::ROUTER | whatami::PEER => {
                         let peers_net = tables.peers_net.as_ref().unwrap();
-                        let local_context = peers_net
-                            .get_idx(
-                                &peers_net
-                                    .get_link(&face.pid)
-                                    .unwrap()
-                                    .get_pid(&routing_context.unwrap())
-                                    .unwrap(),
-                            )
-                            .unwrap()
-                            .index();
+                        let local_context =
+                            peers_net.get_local_context(routing_context.unwrap(), face.link_id);
                         match &res {
                             Some(res) => res.peers_data_routes[local_context].clone(),
                             None => compute_data_route(

zenoh/src/net/routing/queries.rs
@@ -858,16 +858,8 @@ pub async fn route_query(
                 whatami::ROUTER => match face.whatami {
                     whatami::ROUTER => {
                         let routers_net = tables.routers_net.as_ref().unwrap();
-                        let local_context = routers_net
-                            .get_idx(
-                                &routers_net
-                                    .get_link(&face.pid)
-                                    .unwrap()
-                                    .get_pid(&routing_context.unwrap())
-                                    .unwrap(),
-                            )
-                            .unwrap()
-                            .index();
+                        let local_context =
+                            routers_net.get_local_context(routing_context.unwrap(), face.link_id);
                         match Resource::get_resource(prefix, suffix) {
                             Some(res) => res.routers_query_routes[local_context].clone(),
                             None => compute_query_route(
@@ -881,16 +873,8 @@ pub async fn route_query(
                     }
                     whatami::PEER => {
                         let peers_net = tables.peers_net.as_ref().unwrap();
-                        let local_context = peers_net
-                            .get_idx(
-                                &peers_net
-                                    .get_link(&face.pid)
-                                    .unwrap()
-                                    .get_pid(&routing_context.unwrap())
-                                    .unwrap(),
-                            )
-                            .unwrap()
-                            .index();
+                        let local_context =
+                            peers_net.get_local_context(routing_context.unwrap(), face.link_id);
                         match Resource::get_resource(prefix, suffix) {
                             Some(res) => res.peers_query_routes[local_context].clone(),
                             None => compute_query_route(
@@ -910,16 +894,8 @@ pub async fn route_query(
                 whatami::PEER => match face.whatami {
                     whatami::ROUTER | whatami::PEER => {
                         let peers_net = tables.peers_net.as_ref().unwrap();
-                        let local_context = peers_net
-                            .get_idx(
-                                &peers_net
-                                    .get_link(&face.pid)
-                                    .unwrap()
-                                    .get_pid(&routing_context.unwrap())
-                                    .unwrap(),
-                            )
-                            .unwrap()
-                            .index();
+                        let local_context =
+                            peers_net.get_local_context(routing_context.unwrap(), face.link_id);
                         match Resource::get_resource(prefix, suffix) {
                             Some(res) => res.peers_query_routes[local_context].clone(),
                             None => compute_query_route(

commit fef35157f4f7bb8dbea5a946bf31abc8d7743eed

Fix regression introduced by 32a5f3ccd5e3a0f92771ef7e53c7a1512b99852a

zenoh/src/net/routing/network.rs
@@ -85,7 +85,7 @@ pub(crate) struct Network {
     pub(crate) name: String,
     pub(crate) peers_autoconnect: bool,
     pub(crate) idx: NodeIndex,
-    pub(crate) links: Vec<Link>,
+    pub(crate) links: VecMap<Link>,
     pub(crate) trees: Vec<Tree>,
     pub(crate) graph: petgraph::stable_graph::StableUnGraph<Node, f64>,
     pub(crate) orchestrator: SessionOrchestrator,
@@ -111,7 +111,7 @@ impl Network {
             name,
             peers_autoconnect,
             idx,
-            links: vec![],
+            links: VecMap::new(),
             trees: vec![Tree {
                 parent: None,
                 childs: vec![],
@@ -152,7 +152,7 @@ impl Network {
     #[inline]
     pub(crate) fn get_link_from_pid(&self, pid: &PeerId) -> Option<&Link> {
         self.links
-            .iter()
+            .values()
             .find(|link| link.session.get_pid().unwrap() == *pid)
     }
 
@@ -166,7 +166,7 @@ impl Network {
     fn add_node(&mut self, node: Node) -> NodeIndex {
         let pid = node.pid.clone();
         let idx = self.graph.add_node(node);
-        for link in &mut self.links {
+        for link in self.links.values_mut() {
             if let Some((psid, _)) = link.mappings.iter().find(|(_, p)| **p == pid) {
                 link.local_mappings.insert(psid, idx.index() as ZInt);
             }
@@ -235,7 +235,7 @@ impl Network {
         P: FnMut(&Link) -> bool,
     {
         let msg = self.make_msg(idxs).await;
-        for link in &self.links {
+        for link in self.links.values() {
             if predicate(link) {
                 log::trace!(
                     "{} Send to {} {:?}",
@@ -275,7 +275,7 @@ impl Network {
         let links = &mut self.links;
 
         let src_link = match links
-            .iter_mut()
+            .values_mut()
             .find(|link| link.session.get_pid().unwrap() == src)
         {
             Some(link) => link,
@@ -495,7 +495,7 @@ impl Network {
                 .into_iter()
                 .map(|(_, idx1, _new_node)| (idx1, true))
                 .collect::<Vec<(NodeIndex, bool)>>();
-            for link in &self.links {
+            for link in self.links.values() {
                 let link_pid = link.session.get_pid().unwrap();
                 if link_pid != src {
                     let updated_idxs: Vec<(NodeIndex, bool)> = updated_idxs
@@ -525,7 +525,14 @@ impl Network {
     }
 
     pub(crate) async fn add_link(&mut self, session: Session) -> usize {
-        self.links.push(Link::new(session.clone()));
+        let free_index = {
+            let mut i = 0;
+            while self.links.contains_key(i) {
+                i += 1;
+            }
+            i
+        };
+        self.links.insert(free_index, Link::new(session.clone()));
 
         let pid = session.get_pid().unwrap();
         let whatami = session.get_whatami().unwrap();
@@ -566,14 +573,14 @@ impl Network {
 
         let idxs = self.graph.node_indices().map(|i| (i, true)).collect();
         self.send_on_link(idxs, &session).await;
-        self.links.len() - 1
+        free_index
     }
 
     pub(crate) async fn remove_link(&mut self, session: &Session) -> Vec<(NodeIndex, Node)> {
         let pid = session.get_pid().unwrap();
         log::trace!("{} remove_link {}", self.name, pid);
         self.links
-            .retain(|link| link.session.get_pid().unwrap() != pid);
+            .retain(|_, link| link.session.get_pid().unwrap() != pid);
         self.graph[self.idx].links.retain(|link| *link != pid);
 
         let idx = self.get_idx(&pid).unwrap();
@@ -586,7 +593,7 @@ impl Network {
 
         let links = self
             .links
-            .iter()
+            .values()
             .map(|link| {
                 self.get_idx(&link.session.get_pid().unwrap())
                     .unwrap()
@@ -608,7 +615,7 @@ impl Network {
             None,
         );
 
-        for link in &self.links {
+        for link in self.links.values() {
             if let Err(e) = link.session.handle_message(msg.clone()).await {
                 log::error!("{} Error sending LinkStateList: {}", self.name, e);
             }