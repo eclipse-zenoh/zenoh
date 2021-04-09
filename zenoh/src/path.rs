e92594a236b4e7a03d784512ef20fedfd16bc484
jerry73204 <jerry73204@gmail.com>
2021-04-09 15:15:15 +0800

    Support "{path,pathexpr} / string" operator

--- zenoh/src/path.rs
+++ zenoh/src/path.rs
@@ -15,6 +15,7 @@ use crate::net::ResKey;
 use regex::Regex;
 use std::convert::TryFrom;
 use std::fmt;
+use std::ops::Div;
 use zenoh_util::core::{ZError, ZErrorKind, ZResult};
 use zenoh_util::zerror;
 
@@ -103,6 +104,38 @@ impl Path {
     }
 }
 
+impl Div<String> for Path {
+    type Output = Path;
+
+    fn div(self, rhs: String) -> Self::Output {
+        &self / rhs
+    }
+}
+
+impl Div<&str> for Path {
+    type Output = Path;
+
+    fn div(self, rhs: &str) -> Self::Output {
+        &self / rhs
+    }
+}
+
+impl Div<String> for &Path {
+    type Output = Path;
+
+    fn div(self, rhs: String) -> Self::Output {
+        Path::try_from(format!("{}/{}", self, rhs)).unwrap()
+    }
+}
+
+impl Div<&str> for &Path {
+    type Output = Path;
+
+    fn div(self, rhs: &str) -> Self::Output {
+        Path::try_from(format!("{}/{}", self, rhs)).unwrap()
+    }
+}
+
 impl fmt::Display for Path {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "{}", self.p)
@@ -139,6 +172,15 @@ impl From<&Path> for ResKey {
 mod tests {
     use super::*;
     use std::convert::TryInto;
+
+    #[test]
+    fn path_div() {
+        assert_eq!(Path::try_from("a").unwrap() / "b", Path { p: "a/b".into() });
+        assert_eq!(
+            Path::try_from("a").unwrap() / String::from("b"),
+            Path { p: "a/b".into() }
+        );
+    }
 
     #[test]
     fn test_path() {
