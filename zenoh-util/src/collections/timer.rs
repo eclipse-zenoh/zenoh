84464aa5af2ce37ca3261c5137a499cf64887987
Luca Cominardi <luca.cominardi@gmail.com>
2021-06-01 18:47:10 +0200

    Cleanup and tidings of zenoh-util code and tests

--- zenoh-util/src/collections/timer.rs
+++ zenoh-util/src/collections/timer.rs
@@ -254,3 +254,148 @@ impl Default for Timer {
         Self::new()
     }
 }
+
+mod tests {
+    #[test]
+    fn timer() {
+        use super::{Timed, TimedEvent, Timer};
+        use async_std::sync::Arc;
+        use async_std::task;
+        use async_trait::async_trait;
+        use std::sync::atomic::{AtomicUsize, Ordering};
+        use std::time::{Duration, Instant};
+
+        #[derive(Clone)]
+        struct MyEvent {
+            counter: Arc<AtomicUsize>,
+        }
+
+        #[async_trait]
+        impl Timed for MyEvent {
+            async fn run(&mut self) {
+                self.counter.fetch_add(1, Ordering::SeqCst);
+            }
+        }
+
+        async fn run() {
+            // Create the timer
+            let mut timer = Timer::new();
+
+            // Counter for testing
+            let counter = Arc::new(AtomicUsize::new(0));
+
+            // Create my custom event
+            let myev = MyEvent {
+                counter: counter.clone(),
+            };
+
+            // Default testing interval: 1 s
+            let interval = Duration::from_secs(1);
+
+            /* [1] */
+            println!("Timer [1]: Once event and run");
+            // Fire a once timed event
+            let now = Instant::now();
+            let event = TimedEvent::once(now + (2 * interval), myev.clone());
+
+            // Add the event to the timer
+            timer.add(event).await;
+
+            // Wait for the event to occur
+            task::sleep(3 * interval).await;
+
+            // Load and reset the counter value
+            let value = counter.swap(0, Ordering::SeqCst);
+            assert_eq!(value, 1);
+
+            /* [2] */
+            println!("Timer [2]: Once event and defuse");
+            // Fire a once timed event and defuse it before it is executed
+            let now = Instant::now();
+            let event = TimedEvent::once(now + (2 * interval), myev.clone());
+            let handle = event.get_handle();
+
+            // Add the event to the timer
+            timer.add(event).await;
+            //
+            handle.defuse();
+
+            // Wait for the event to occur
+            task::sleep(3 * interval).await;
+
+            // Load and reset the counter value
+            let value = counter.swap(0, Ordering::SeqCst);
+            assert_eq!(value, 0);
+
+            /* [3] */
+            println!("Timer [3]: Periodic event run and defuse");
+            // Number of events to occur
+            let amount: usize = 3;
+
+            // Half the waiting interval for granularity reasons
+            let to_elapse = (2 * amount as u32) * interval;
+
+            // Fire a periodic event
+            let event = TimedEvent::periodic(2 * interval, myev.clone());
+            let handle = event.get_handle();
+
+            // Add the event to the timer
+            timer.add(event).await;
+
+            // Wait for the events to occur
+            task::sleep(to_elapse + interval).await;
+
+            // Load and reset the counter value
+            let value = counter.swap(0, Ordering::SeqCst);
+            assert_eq!(value, amount);
+
+            // Defuse the event (check if twice defusing don't cause troubles)
+            handle.clone().defuse();
+            handle.defuse();
+
+            // Wait a bit more to verify that not more events have been fired
+            task::sleep(to_elapse).await;
+
+            // Load and reset the counter value
+            let value = counter.swap(0, Ordering::SeqCst);
+            assert_eq!(value, 0);
+
+            /* [4] */
+            println!("Timer [4]: Periodic event and stop/start timer");
+            // Fire a periodic event
+            let event = TimedEvent::periodic(2 * interval, myev);
+
+            // Add the event to the timer
+            timer.add(event).await;
+
+            // Wait for the events to occur
+            task::sleep(to_elapse + interval).await;
+
+            // Load and reset the counter value
+            let value = counter.swap(0, Ordering::SeqCst);
+            assert_eq!(value, amount);
+
+            // Stop the timer
+            timer.stop().await;
+
+            // Wait some time
+            task::sleep(to_elapse).await;
+
+            // Load and reset the counter value
+            let value = counter.swap(0, Ordering::SeqCst);
+            assert_eq!(value, 0);
+
+            // Restart the timer
+            timer.start().await;
+
+            // Wait for the events to occur
+            task::sleep(to_elapse).await;
+
+            // Load and reset the counter value
+            let value = counter.swap(0, Ordering::SeqCst);
+            assert_eq!(value, amount);
+        }
+
+        task::block_on(run());
+    }
+}
--- zenoh-util/src/crypto/cipher.rs
+++ zenoh-util/src/crypto/cipher.rs
@@ -67,3 +67,60 @@ impl BlockCipher {
         Ok(bytes)
     }
 }
+
+mod tests {
+    #[test]
+    fn cipher() {
+        use super::{BlockCipher, PseudoRng};
+        use rand::{RngCore, SeedableRng};
+
+        fn encrypt_decrypt(cipher: &BlockCipher, prng: &mut PseudoRng) {
+            println!("\n[1]");
+            let t1 = "A".as_bytes().to_vec();
+            println!("Clear: {:?}", t1);
+            let encrypted = cipher.encrypt(t1.clone(), prng);
+            println!("Encrypted: {:?}", encrypted);
+            let decrypted = cipher.decrypt(encrypted).unwrap();
+            println!("Decrypted: {:?}", decrypted);
+            assert_eq!(&t1[..], &decrypted[..t1.len()]);
+
+            println!("\n[2]");
+            let t2 = "Short string".as_bytes().to_vec();
+            println!("Clear: {:?}", t2);
+            let encrypted = cipher.encrypt(t2.clone(), prng);
+            println!("Encrypted: {:?}", encrypted);
+            let decrypted = cipher.decrypt(encrypted).unwrap();
+            println!("Decrypted: {:?}", decrypted);
+            assert_eq!(&t2[..], &decrypted[..t2.len()]);
+
+            println!("\n[3]");
+            let t3 = "This is a medium string with some text".as_bytes().to_vec();
+            println!("Clear: {:?}", t3);
+            let encrypted = cipher.encrypt(t3.clone(), prng);
+            println!("Encrypted: {:?}", encrypted);
+            let decrypted = cipher.decrypt(encrypted).unwrap();
+            println!("Decrypted: {:?}", decrypted);
+            assert_eq!(&t3[..], &decrypted[..t3.len()]);
+
+            println!("\n[4]");
+            let t4 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.".as_bytes().to_vec();
+            println!("Clear: {:?}", t4);
+            let encrypted = cipher.encrypt(t4.clone(), prng);
+            println!("Encrypted: {:?}", encrypted);
+            let decrypted = cipher.decrypt(encrypted).unwrap();
+            println!("Decrypted: {:?}", decrypted);
+            assert_eq!(&t4[..], &decrypted[..t4.len()]);
+        }
+
+        const RUN: usize = 16;
+
+        let mut prng = PseudoRng::from_entropy();
+        let mut key = [0u8; BlockCipher::BLOCK_SIZE];
+        prng.fill_bytes(&mut key);
+        let cipher = BlockCipher::new(key);
+
+        for _ in 0..RUN {
+            encrypt_decrypt(&cipher, &mut prng);
+        }
+    }
+}
