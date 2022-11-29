8b78e6a090cad12a8f0271667025085538cfb389
OlivierHecart <olivier.hecart@adlinktech.com>
2022-11-29 15:22:18 +0100

    Add pub/query allowed_destination option (#382)

--- zenoh/src/publication.rs
+++ zenoh/src/publication.rs
@@ -99,6 +99,15 @@ impl PutBuilder<'_, '_> {
         self
     }
 
+    /// Restrict the matching subscribers that will receive the published data
+    /// to the ones that have the given [`Locality`](crate::prelude::Locality).
+    #[zenoh_core::unstable]
+    #[inline]
+    pub fn allowed_destination(mut self, destination: Locality) -> Self {
+        self.publisher = self.publisher.allowed_destination(destination);
+        self
+    }
+
     pub fn kind(mut self, kind: SampleKind) -> Self {
         self.kind = kind;
         self
@@ -135,23 +144,27 @@ impl SyncResolve for PutBuilder<'_, '_> {
         info.timestamp = publisher.session.runtime.new_timestamp();
         let data_info = if info.has_options() { Some(info) } else { None };
 
-        primitives.send_data(
-            &key_expr.to_wire(&publisher.session),
-            value.payload.clone(),
-            Channel {
-                priority: publisher.priority.into(),
-                reliability: Reliability::Reliable, // @TODO: need to check subscriptions to determine the right reliability value
-            },
-            publisher.congestion_control,
-            data_info.clone(),
-            None,
-        );
-        publisher.session.handle_data(
-            true,
-            &key_expr.to_wire(&publisher.session),
-            data_info,
-            value.payload,
-        );
+        if publisher.destination != Locality::SessionLocal {
+            primitives.send_data(
+                &key_expr.to_wire(&publisher.session),
+                value.payload.clone(),
+                Channel {
+                    priority: publisher.priority.into(),
+                    reliability: Reliability::Reliable, // @TODO: need to check subscriptions to determine the right reliability value
+                },
+                publisher.congestion_control,
+                data_info.clone(),
+                None,
+            );
+        }
+        if publisher.destination != Locality::Remote {
+            publisher.session.handle_data(
+                true,
+                &key_expr.to_wire(&publisher.session),
+                data_info,
+                value.payload,
+            );
+        }
         Ok(())
     }
 }
@@ -206,6 +219,7 @@ pub struct Publisher<'a> {
     pub(crate) key_expr: KeyExpr<'a>,
     pub(crate) congestion_control: CongestionControl,
     pub(crate) priority: Priority,
+    pub(crate) destination: Locality,
 }
 
 impl<'a> Publisher<'a> {
@@ -224,6 +238,15 @@ impl<'a> Publisher<'a> {
     #[inline]
     pub fn priority(mut self, priority: Priority) -> Self {
         self.priority = priority;
+        self
+    }
+
+    /// Restrict the matching subscribers that will receive the published data
+    /// to the ones that have the given [`Locality`](crate::prelude::Locality).
+    #[zenoh_core::unstable]
+    #[inline]
+    pub fn allowed_destination(mut self, destination: Locality) -> Self {
+        self.destination = destination;
         self
     }
 
@@ -401,23 +424,27 @@ impl SyncResolve for Publication<'_> {
         info.timestamp = publisher.session.runtime.new_timestamp();
         let data_info = if info.has_options() { Some(info) } else { None };
 
-        primitives.send_data(
-            &publisher.key_expr.to_wire(&publisher.session),
-            value.payload.clone(),
-            Channel {
-                priority: publisher.priority.into(),
-                reliability: Reliability::Reliable, // @TODO: need to check subscriptions to determine the right reliability value
-            },
-            publisher.congestion_control,
-            data_info.clone(),
-            None,
-        );
-        publisher.session.handle_data(
-            true,
-            &publisher.key_expr.to_wire(&publisher.session),
-            data_info,
-            value.payload,
-        );
+        if publisher.destination != Locality::SessionLocal {
+            primitives.send_data(
+                &publisher.key_expr.to_wire(&publisher.session),
+                value.payload.clone(),
+                Channel {
+                    priority: publisher.priority.into(),
+                    reliability: Reliability::Reliable, // @TODO: need to check subscriptions to determine the right reliability value
+                },
+                publisher.congestion_control,
+                data_info.clone(),
+                None,
+            );
+        }
+        if publisher.destination != Locality::Remote {
+            publisher.session.handle_data(
+                true,
+                &publisher.key_expr.to_wire(&publisher.session),
+                data_info,
+                value.payload,
+            );
+        }
         Ok(())
     }
 }
@@ -480,6 +507,7 @@ pub struct PublisherBuilder<'a, 'b: 'a> {
     pub(crate) key_expr: ZResult<KeyExpr<'b>>,
     pub(crate) congestion_control: CongestionControl,
     pub(crate) priority: Priority,
+    pub(crate) destination: Locality,
 }
 
 impl<'a, 'b> Clone for PublisherBuilder<'a, 'b> {
@@ -492,6 +520,7 @@ impl<'a, 'b> Clone for PublisherBuilder<'a, 'b> {
             },
             congestion_control: self.congestion_control,
             priority: self.priority,
+            destination: self.destination,
         }
     }
 }
@@ -508,6 +537,15 @@ impl<'a, 'b> PublisherBuilder<'a, 'b> {
     #[inline]
     pub fn priority(mut self, priority: Priority) -> Self {
         self.priority = priority;
+        self
+    }
+
+    /// Restrict the matching subscribers that will receive the published data
+    /// to the ones that have the given [`Locality`](crate::prelude::Locality).
+    #[zenoh_core::unstable]
+    #[inline]
+    pub fn allowed_destination(mut self, destination: Locality) -> Self {
+        self.destination = destination;
         self
     }
 }
@@ -555,6 +593,7 @@ impl<'a, 'b> SyncResolve for PublisherBuilder<'a, 'b> {
             key_expr,
             congestion_control: self.congestion_control,
             priority: self.priority,
+            destination: self.destination,
         };
         log::trace!("publish({:?})", publisher.key_expr);
         Ok(publisher)
