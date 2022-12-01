619cef65bd892169e0b1ddacbe4d4c1972581c97
Pierre Avital <pierre.avital@adlinktech.com>
2022-12-01 17:34:00 +0100

    fuzzer seems happy, introduce depth instrumented iterator for KETree

--- commons/zenoh-collections/src/keyexpr_tree/keyed_set_tree/tree_iter.rs
+++ commons/zenoh-collections/src/keyexpr_tree/keyed_set_tree/tree_iter.rs
@@ -1,3 +1,5 @@
+use std::num::NonZeroUsize;
+
 use super::*;
 pub struct TreeIter<'a, Children: ChunkMapType<Node>, Node: IKeyExprTreeNode<Weight>, Weight>
 where
@@ -17,6 +19,9 @@ where
             iterators: vec![children.children()],
             _marker: Default::default(),
         }
+    }
+    pub fn with_depth(self) -> DepthInstrumented<Self> {
+        DepthInstrumented(self)
     }
 }
 
@@ -42,6 +47,39 @@ where
                 }
                 None => {
                     self.iterators.pop();
+                }
+            }
+        }
+    }
+}
+
+pub struct DepthInstrumented<T>(T);
+impl<
+        'a,
+        Children: ChunkMapType<Node>,
+        Node: IKeyExprTreeNode<Weight, Children = Children::Assoc> + 'a,
+        Weight,
+    > Iterator for DepthInstrumented<TreeIter<'a, Children, Node, Weight>>
+where
+    Children::Assoc: ChunkMap<Node> + 'a,
+{
+    type Item = (
+        NonZeroUsize,
+        <Children::Assoc as ChunkMap<Node>>::IterItem<'a>,
+    );
+    fn next(&mut self) -> Option<Self::Item> {
+        loop {
+            let depth = self.0.iterators.len();
+            match self.0.iterators.last_mut()?.next() {
+                Some(node) => {
+                    let iterator = unsafe { &*(node.as_node() as *const Node) }
+                        .children()
+                        .children();
+                    self.0.iterators.push(iterator);
+                    return Some((unsafe { NonZeroUsize::new_unchecked(depth) }, node));
+                }
+                None => {
+                    self.0.iterators.pop();
                 }
             }
         }
--- commons/zenoh-collections/src/keyexpr_tree/test.rs
+++ commons/zenoh-collections/src/keyexpr_tree/test.rs
@@ -1,8 +1,11 @@
+use zenoh_protocol_core::key_expr::fuzzer::KeyExprFuzzer;
+
 use super::*;
 use std::{
     collections::HashMap,
     convert::{TryFrom, TryInto},
     fmt::Debug,
+    ops::Deref,
 };
 
 fn insert<'a, K: TryInto<&'a keyexpr>, V: Clone + PartialEq + Debug + 'static>(
@@ -29,21 +32,44 @@ fn insert<'a, K: TryInto<&'a keyexpr>, V: Clone + PartialEq + Debug + 'static>(
     )
 }
 
-#[test]
-fn keyed_set_tree() {
-    const KEYS: [&str; 7] = ["a/b/c", "a/b/c", "a/*/c", "**/c", "**/d", "d/b/c", "**/b/c"];
+fn into_ke(s: &str) -> &keyexpr {
+    keyexpr::new(s).unwrap()
+}
+
+fn test_keyset<K: Deref<Target = keyexpr>>(keys: &[K]) {
     let mut tree = KeyExprTree::new();
     let mut map = HashMap::new();
-    for (v, k) in IntoIterator::into_iter(KEYS).enumerate() {
+    for (v, k) in keys.iter().map(|k| k.deref()).enumerate() {
         insert(&mut tree, &mut map, k, v);
     }
-    println!("ALL NODES");
     for node in tree.tree_iter() {
-        println!("{}: {:?}", node.keyexpr(), node.weight());
         assert_eq!(node.weight(), map.get(&node.keyexpr()).unwrap().as_ref());
     }
-    println!("INTERSECTION a/b/c");
-    for node in tree.intersecting_nodes("a/b/c".try_into().unwrap()) {
-        println!("{}: {:?}", node.keyexpr(), node.weight())
+    for target in keys {
+        let mut expected = HashMap::new();
+        for (k, v) in &map {
+            if target.intersects(k) {
+                assert!(expected.insert(k, v).is_none());
+            }
+        }
+        for node in tree.intersecting_nodes(target) {
+            let ke = node.keyexpr();
+            let weight = node.weight();
+            assert_eq!(expected.get(&ke).unwrap().as_ref(), weight)
+        }
     }
+}
+
+#[test]
+fn keyed_set_tree() {
+    let keys: [&keyexpr; 7] =
+        ["a/b/c", "a/b/c", "a/*/c", "**/c", "**/d", "d/b/c", "**/b/c"].map(into_ke);
+    test_keyset(&keys)
+}
+
+#[test]
+fn fuzz() {
+    let fuzzer = KeyExprFuzzer(rand::thread_rng());
+    let keys = fuzzer.take(1000).collect::<Vec<_>>();
+    test_keyset(&keys)
 }
