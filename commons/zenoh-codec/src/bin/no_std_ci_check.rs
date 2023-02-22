2465dc8da6e44362fa4f103ec592261939156e71
Davide Della Giustina <davide@dellagiustina.com>
2023-02-22 13:29:54 +0000

    Fixed CI checks for protocl/codec

---
+++ commons/zenoh-codec/src/bin/no_std_ci_check.rs
@@ -0,0 +1,28 @@
+//
+// Copyright (c) 2022 ZettaScale Technology
+//
+// This program and the accompanying materials are made available under the
+// terms of the Eclipse Public License 2.0 which is available at
+// http://www.eclipse.org/legal/epl-2.0, or the Apache License, Version 2.0
+// which is available at https://www.apache.org/licenses/LICENSE-2.0.
+//
+// SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
+//
+// Contributors:
+//   ZettaScale Zenoh Team, <zenoh@zettascale.tech>
+//
+
+#![no_std]
+
+#[panic_handler]
+fn dummy_panic_handler(_: &core::panic::PanicInfo) -> ! {
+    loop {}
+}
+
+fn dummy_get_rand(_: &mut [u8]) -> Result<(), getrandom::Error> {
+    Ok(())
+}
+
+fn main() {
+    getrandom::register_custom_getrandom!(dummy_get_rand);
+}
