0e632430911701ec115fd92b89f6db0d7a0f6a74
yuanyuyuan <az6980522@gmail.com>
2023-05-22 14:58:40 +0800

    Add WIP failover_brokering

--- zenoh/tests/routing.rs
+++ zenoh/tests/routing.rs
@@ -25,7 +25,6 @@ use zenoh_result::bail;
 const TIMEOUT: Duration = Duration::from_secs(10);
 const MSG_COUNT: usize = 1_000;
 const MSG_SIZE: [usize; 2] = [1_024, 131_072];
-// const MSG_SIZE: [usize; 1] = [1_024];
 
 macro_rules! ztimeout {
     ($f:expr) => {
@@ -47,6 +46,7 @@ struct Node {
     listen: Vec<String>,
     connect: Vec<String>,
     task: Vec<Task>,
+    config: Option<Config>,
 }
 
 impl Default for Node {
@@ -57,16 +57,18 @@ impl Default for Node {
             listen: vec![],
             connect: vec![],
             task: vec![],
+            config: None,
         }
     }
 }
 
 async fn run_recipe(receipe: impl IntoIterator<Item = Node>) -> Result<()> {
-    let futures = receipe.into_iter().map(|node| async move {
+    let futures = receipe.into_iter().map(|node|
+        async move {
         dbg!(node.name);
 
         // Load the config and build up a session
-        let mut config = Config::default();
+        let mut config = node.config.unwrap_or_else(Config::default);
         config.set_mode(Some(node.mode)).unwrap();
         config.scouting.multicast.set_enabled(Some(false)).unwrap();
         config
@@ -90,8 +92,8 @@ async fn run_recipe(receipe: impl IntoIterator<Item = Node>) -> Result<()> {
         // Each node consists of a specified session associated with tasks to run
         let futs = node.task.into_iter().map(|task| {
 
-            // Each session spawns several given task(s)
-            let c_session = session.clone();
+            // Multiple tasks share the session
+            let session = session.clone();
 
             match task {
 
@@ -99,7 +101,7 @@ async fn run_recipe(receipe: impl IntoIterator<Item = Node>) -> Result<()> {
                 Task::Sub(topic, expected_size) => {
                     async_std::task::spawn(async move {
                         let sub =
-                            ztimeout!(c_session.declare_subscriber(&topic).res_async())?;
+                            ztimeout!(session.declare_subscriber(&topic).res_async())?;
                         let mut counter = 0;
                         while let Ok(sample) = sub.recv_async().await {
                             let recv_size = sample.value.payload.len();
@@ -121,7 +123,7 @@ async fn run_recipe(receipe: impl IntoIterator<Item = Node>) -> Result<()> {
                     async_std::task::spawn(async move {
                         loop {
                             // async_std::task::sleep(Duration::from_millis(300)).await;
-                            ztimeout!(c_session
+                            ztimeout!(session
                                 .put(&topic, vec![0u8; payload_size])
                                 .congestion_control(CongestionControl::Block)
                                 .res_async())?;
@@ -147,6 +149,44 @@ async fn run_recipe(receipe: impl IntoIterator<Item = Node>) -> Result<()> {
 
 #[test]
 fn gossip() -> Result<()> {
+    async_std::task::block_on(async {
+        zasync_executor_init!();
+        let locator = String::from("tcp/127.0.0.1:17448");
+        let topic = String::from("testTopic");
+
+        // Gossip test
+        let recipe = [
+            Node {
+                name: "Peer0".into(),
+                mode: WhatAmI::Peer,
+                listen: vec![locator.clone()],
+                task: vec![Task::Sleep(Duration::from_secs(2))],
+                ..Default::default()
+            },
+            Node {
+                name: "Peer1".into(),
+                connect: vec![locator.clone()],
+                mode: WhatAmI::Peer,
+                task: vec![Task::Pub(topic.clone(), 8)],
+                ..Default::default()
+            },
+            Node {
+                name: "Peer2".into(),
+                mode: WhatAmI::Peer,
+                connect: vec![locator.clone()],
+                task: vec![Task::Sub(topic.clone(), 8)],
+                ..Default::default()
+            },
+        ];
+        run_recipe(recipe).await?;
+        println!("Gossip test passed.");
+        Result::Ok(())
+    })?;
+    Ok(())
+}
+
+#[test]
+fn failover_brokering() -> Result<()> {
     async_std::task::block_on(async {
         zasync_executor_init!();
         let locator = String::from("tcp/127.0.0.1:17448");
