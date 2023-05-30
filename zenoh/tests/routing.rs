57683470cced4b97de758b93028686feb724c545
yuanyuyuan <az6980522@gmail.com>
2023-05-30 17:57:29 +0800

    Add the warmup time, the recipe info, and the p2p recipe

--- zenoh/tests/routing.rs
+++ zenoh/tests/routing.rs
@@ -47,8 +47,8 @@ enum Task {
 impl Task {
     async fn run(&self, session: Arc<Session>, terminated: Arc<AtomicBool>) -> Result<()> {
         match self {
-            Self::Sub(topic, expected_size) => {
-                let sub = ztimeout!(session.declare_subscriber(topic).res_async())?;
+            Self::Sub(ke, expected_size) => {
+                let sub = ztimeout!(session.declare_subscriber(ke).res_async())?;
                 let mut counter = 0;
                 while let Ok(sample) = sub.recv_async().await {
                     let recv_size = sample.value.payload.len();
@@ -63,11 +63,11 @@ impl Task {
                 }
             }
 
-            Self::Pub(topic, payload_size) => {
+            Self::Pub(ke, payload_size) => {
                 while !terminated.load(Ordering::Relaxed) {
                     // async_std::task::sleep(Duration::from_millis(300)).await;
                     ztimeout!(session
-                        .put(topic, vec![0u8; *payload_size])
+                        .put(ke, vec![0u8; *payload_size])
                         .congestion_control(CongestionControl::Block)
                         .res_async())?;
                 }
@@ -102,6 +102,7 @@ struct Node {
     connect: Vec<String>,
     task: ConcurrentTask,
     config: Option<Config>,
+    warmup: Duration,
 }
 
 impl Default for Node {
@@ -113,78 +114,100 @@ impl Default for Node {
             connect: vec![],
             task: vec![],
             config: None,
+            warmup: Duration::from_secs(0),
         }
     }
 }
 
-async fn run_recipe(receipe: impl IntoIterator<Item = Node>) -> Result<()> {
-    let terminated = Arc::new(AtomicBool::default());
+#[derive(Debug, Clone)]
+struct Recipe {
+    nodes: Vec<Node>,
+}
 
-    let futures = receipe.into_iter().map(|node| {
-        log::info!("Node: {} started.", &node.name);
+impl std::fmt::Display for Recipe {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        let names: Vec<_> = self.nodes.iter().map(|node| node.name.clone()).collect();
+        write!(f, "[{}]", names.join(", "))
+    }
+}
 
-        // Multiple nodes share the same terminated flag
-        let terminated = terminated.clone();
+impl Recipe {
+    fn new(nodes: impl IntoIterator<Item = Node>) -> Self {
+        let nodes = nodes.into_iter().collect();
+        Self { nodes }
+    }
 
-        async move {
-            dbg!(&node.name);
+    async fn run(&self) -> Result<()> {
+        let terminated = Arc::new(AtomicBool::default());
 
-            // Load the config and build up a session
-            let config = {
-                let mut config = node.config.unwrap_or_default();
-                config.set_mode(Some(node.mode)).unwrap();
-                config.scouting.multicast.set_enabled(Some(false)).unwrap();
-                config
-                    .listen
-                    .set_endpoints(node.listen.iter().map(|x| x.parse().unwrap()).collect())
-                    .unwrap();
-                config
-                    .connect
-                    .set_endpoints(node.connect.iter().map(|x| x.parse().unwrap()).collect())
-                    .unwrap();
-                config
-            };
+        let futures = self.nodes.clone().into_iter().map(|node| {
+            log::info!("Node: {} started.", &node.name);
 
-            // In case of client can't connect to some peers/routers
-            let session = if let Ok(session) = zenoh::open(config.clone()).res_async().await {
-                session.into_arc()
-            } else {
-                log::info!("Sleep and retry to connect to the endpoint...");
-                async_std::task::sleep(Duration::from_secs(1)).await;
-                zenoh::open(config).res_async().await?.into_arc()
-            };
+            // Multiple nodes share the same terminated flag
+            let terminated = terminated.clone();
 
-            // Each node consists of a specified session associated with tasks to run
-            let futs = node.task.into_iter().map(|seq_tasks| {
-                // Multiple tasks share the session
-                let session = session.clone();
-                let terminated = terminated.clone();
+            async move {
+                dbg!(&node.name);
 
-                async_std::task::spawn(async move {
-                    for t in seq_tasks {
-                        t.run(session.clone(), terminated.clone()).await?;
-                    }
-                    Result::Ok(())
-                })
-            });
+                // Load the config and build up a session
+                let config = {
+                    let mut config = node.config.unwrap_or_default();
+                    config.set_mode(Some(node.mode)).unwrap();
+                    config.scouting.multicast.set_enabled(Some(false)).unwrap();
+                    config
+                        .listen
+                        .set_endpoints(node.listen.iter().map(|x| x.parse().unwrap()).collect())
+                        .unwrap();
+                    config
+                        .connect
+                        .set_endpoints(node.connect.iter().map(|x| x.parse().unwrap()).collect())
+                        .unwrap();
+                    config
+                };
 
-            // let (state, _, _) = select_all(futs).await;
-            // state
-            try_join_all(futs).await?;
-            Arc::try_unwrap(session)
-                .unwrap()
-                .close()
-                .res_async()
-                .await?;
-            log::info!("Node: {} is closed.", &node.name);
-            Result::Ok(())
-        }
-    });
+                async_std::task::sleep(node.warmup).await;
 
-    // let (state, _, _) = select_all(futures).timeout(TIMEOUT).await?;
-    // state
-    try_join_all(futures).timeout(TIMEOUT).await??;
-    Ok(())
+                // In case of client can't connect to some peers/routers
+                let session = if let Ok(session) = zenoh::open(config.clone()).res_async().await {
+                    session.into_arc()
+                } else {
+                    log::info!("Sleep and retry to connect to the endpoint...");
+                    async_std::task::sleep(Duration::from_secs(1)).await;
+                    zenoh::open(config).res_async().await?.into_arc()
+                };
+
+                // Each node consists of a specified session associated with tasks to run
+                let futs = node.task.into_iter().map(|seq_tasks| {
+                    // Multiple tasks share the session
+                    let session = session.clone();
+                    let terminated = terminated.clone();
+
+                    async_std::task::spawn(async move {
+                        for t in seq_tasks {
+                            t.run(session.clone(), terminated.clone()).await?;
+                        }
+                        Result::Ok(())
+                    })
+                });
+
+                // let (state, _, _) = select_all(futs).await;
+                // state
+                try_join_all(futs).await?;
+                Arc::try_unwrap(session)
+                    .unwrap()
+                    .close()
+                    .res_async()
+                    .await?;
+                log::info!("Node: {} is closed.", &node.name);
+                Result::Ok(())
+            }
+        });
+
+        // let (state, _, _) = select_all(futures).timeout(TIMEOUT).await?;
+        // state
+        try_join_all(futures).timeout(TIMEOUT).await??;
+        Ok(())
+    }
 }
 
 #[test]
@@ -194,13 +217,13 @@ fn gossip() -> Result<()> {
         env_logger::try_init()?;
 
         let locator = String::from("tcp/127.0.0.1:17448");
-        let topic = String::from("testTopicGossip");
+        let ke = String::from("testKeyExprGossip");
         let msg_size = 8;
 
         // Gossip test
-        let recipe = [
+        let recipe = Recipe::new([
             Node {
-                name: "Peer0".into(),
+                name: format!("RTR {}", WhatAmI::Peer),
                 mode: WhatAmI::Peer,
                 listen: vec![locator.clone()],
                 task: ConcurrentTask::from([SequentialTask::from([Task::Sleep(
@@ -209,28 +232,30 @@ fn gossip() -> Result<()> {
                 ..Default::default()
             },
             Node {
-                name: "Peer1".into(),
+                name: format!("Pub {}", WhatAmI::Peer),
                 connect: vec![locator.clone()],
                 mode: WhatAmI::Peer,
                 task: ConcurrentTask::from([SequentialTask::from([
                     Task::Sleep(Duration::from_millis(2000)),
-                    Task::Pub(topic.clone(), msg_size),
+                    Task::Pub(ke.clone(), msg_size),
                 ])]),
                 ..Default::default()
             },
             Node {
-                name: "Peer2".into(),
+                name: format!("Sub {}", WhatAmI::Peer),
                 mode: WhatAmI::Peer,
                 connect: vec![locator.clone()],
                 task: ConcurrentTask::from([SequentialTask::from([
                     Task::Sleep(Duration::from_millis(2000)),
-                    Task::Sub(topic, msg_size),
+                    Task::Sub(ke, msg_size),
                     Task::Terminate,
                 ])]),
                 ..Default::default()
             },
-        ];
-        run_recipe(recipe).await?;
+        ]);
+
+        log::info!("[Recipe]: {}", &recipe);
+        recipe.run().await?;
         println!("Gossip test passed.");
         Result::Ok(())
     })?;
@@ -244,7 +269,7 @@ fn static_failover_brokering() -> Result<()> {
         // env_logger::try_init()?;
 
         let locator = String::from("tcp/127.0.0.1:17449");
-        let topic = String::from("testTopicStaticFailoverBrokering");
+        let ke = String::from("testKeyExprStaticFailoverBrokering");
         let msg_size = 8;
 
         let disable_autoconnect_config = || {
@@ -259,38 +284,39 @@ fn static_failover_brokering() -> Result<()> {
             Some(config)
         };
 
-        let recipe = [
+        let recipe = Recipe::new([
             Node {
-                name: "Router".into(),
+                name: format!("RTR {}", WhatAmI::Router),
                 mode: WhatAmI::Router,
                 listen: vec![locator.clone()],
                 task: ConcurrentTask::from([SequentialTask::from([Task::Wait])]),
                 ..Default::default()
             },
             Node {
-                name: "Peer1".into(),
+                name: format!("Pub {}", WhatAmI::Peer),
                 mode: WhatAmI::Peer,
                 connect: vec![locator.clone()],
                 config: disable_autoconnect_config(),
                 task: ConcurrentTask::from([SequentialTask::from([Task::Pub(
-                    topic.clone(),
+                    ke.clone(),
                     msg_size,
                 )])]),
                 ..Default::default()
             },
             Node {
-                name: "Peer2".into(),
+                name: format!("Sub {}", WhatAmI::Peer),
                 mode: WhatAmI::Peer,
                 connect: vec![locator.clone()],
                 config: disable_autoconnect_config(),
                 task: ConcurrentTask::from([SequentialTask::from([
-                    Task::Sub(topic.clone(), msg_size),
+                    Task::Sub(ke.clone(), msg_size),
                     Task::Terminate,
                 ])]),
                 ..Default::default()
             },
-        ];
-        run_recipe(recipe).await?;
+        ]);
+        log::info!("[Recipe]: {}", &recipe);
+        recipe.run().await?;
         println!("Static failover brokering test passed.");
         Result::Ok(())
     })?;
@@ -314,33 +340,33 @@ fn pubsub_combination() -> Result<()> {
             .flat_map(|(n1, n2)| MSG_SIZE.map(|s| (n1, n2, s)))
             .flat_map(|(node1_mode, node2_mode, msg_size)| {
                 idx += 1;
-                let topic = format!("pubsub_combination_topic_{idx}");
+                let ke = format!("pubsub_combination_keyexpr_{idx}");
                 let locator = format!("tcp/127.0.0.1:{}", base_port + idx);
 
                 let recipe = vec![
                     Node {
-                        name: "Router".into(),
+                        name: format!("RTR {}", WhatAmI::Router),
                         mode: WhatAmI::Router,
                         listen: vec![locator.clone()],
                         task: ConcurrentTask::from([SequentialTask::from([Task::Wait])]),
                         ..Default::default()
                     },
                     Node {
-                        name: "Node1".into(),
-                        connect: vec![locator.clone()],
+                        name: format!("Pub {node1_mode}"),
                         mode: node1_mode,
+                        connect: vec![locator.clone()],
                         task: ConcurrentTask::from([SequentialTask::from([Task::Pub(
-                            topic.clone(),
+                            ke.clone(),
                             msg_size,
                         )])]),
                         ..Default::default()
                     },
                     Node {
-                        name: "Node2".into(),
+                        name: format!("Sub {node2_mode}"),
                         mode: node2_mode,
                         connect: vec![locator],
                         task: ConcurrentTask::from([SequentialTask::from([
-                            Task::Sub(topic, msg_size),
+                            Task::Sub(ke, msg_size),
                             Task::Terminate,
                         ])]),
                         ..Default::default()
@@ -349,22 +375,97 @@ fn pubsub_combination() -> Result<()> {
 
                 // All permuations
                 vec![
-                    vec![recipe[0].clone(), recipe[1].clone(), recipe[2].clone()],
-                    vec![recipe[0].clone(), recipe[2].clone(), recipe[1].clone()],
-                    vec![recipe[1].clone(), recipe[0].clone(), recipe[2].clone()],
-                    vec![recipe[1].clone(), recipe[2].clone(), recipe[0].clone()],
-                    vec![recipe[2].clone(), recipe[0].clone(), recipe[1].clone()],
-                    vec![recipe[2].clone(), recipe[1].clone(), recipe[0].clone()],
+                    Recipe::new([recipe[0].clone(), recipe[1].clone(), recipe[2].clone()]),
+                    Recipe::new([recipe[0].clone(), recipe[2].clone(), recipe[1].clone()]),
+                    Recipe::new([recipe[1].clone(), recipe[0].clone(), recipe[2].clone()]),
+                    Recipe::new([recipe[1].clone(), recipe[2].clone(), recipe[0].clone()]),
+                    Recipe::new([recipe[2].clone(), recipe[0].clone(), recipe[1].clone()]),
+                    Recipe::new([recipe[2].clone(), recipe[1].clone(), recipe[0].clone()]),
                 ]
             });
 
         for recipe in recipe_list {
-            run_recipe(recipe).await?;
+            log::info!("[Recipe]: {}", &recipe);
+            recipe.run().await?;
         }
 
         // try_join_all(recipe_list.map(|r| run_recipe(r))).await?;
 
         println!("Pub/sub combination test passed.");
+        Result::Ok(())
+    })?;
+    Ok(())
+}
+
+#[test]
+fn p2p_combination() -> Result<()> {
+    async_std::task::block_on(async {
+        zasync_executor_init!();
+        // env_logger::try_init()?;
+
+        #[derive(Clone, Copy)]
+        struct IsFirstListen(bool);
+
+        let modes = [
+            (WhatAmI::Client, WhatAmI::Peer, IsFirstListen(false)),
+            (WhatAmI::Peer, WhatAmI::Client, IsFirstListen(true)),
+            (WhatAmI::Peer, WhatAmI::Peer, IsFirstListen(true)),
+            (WhatAmI::Peer, WhatAmI::Peer, IsFirstListen(false)),
+        ];
+
+        let mut idx = 0;
+        let base_port = 17470;
+        let recipe_list = modes
+            .into_iter()
+            .flat_map(|(n1, n2, who)| MSG_SIZE.map(|s| (n1, n2, who, s)))
+            .map(|(node1_mode, node2_mode, who, msg_size)| {
+                idx += 1;
+                let ke = format!("p2p_combination_keyexpr_{idx}");
+
+                let (node1_listen_connect, node2_listen_connect) = {
+                    let locator = format!("tcp/127.0.0.1:{}", base_port + idx);
+                    let listen = vec![locator];
+                    let connect = vec![];
+
+                    if let IsFirstListen(true) = who {
+                        ((listen.clone(), connect.clone()), (connect, listen))
+                    } else {
+                        ((connect.clone(), listen.clone()), (listen, connect))
+                    }
+                };
+
+                Recipe::new([
+                    Node {
+                        name: format!("Pub {node1_mode}"),
+                        mode: node1_mode,
+                        listen: node1_listen_connect.0,
+                        connect: node1_listen_connect.1,
+                        task: ConcurrentTask::from([SequentialTask::from([Task::Pub(
+                            ke.clone(),
+                            msg_size,
+                        )])]),
+                        ..Default::default()
+                    },
+                    Node {
+                        name: format!("Sub {node2_mode}"),
+                        mode: node2_mode,
+                        listen: node2_listen_connect.0,
+                        connect: node2_listen_connect.1,
+                        task: ConcurrentTask::from([SequentialTask::from([
+                            Task::Sub(ke, msg_size),
+                            Task::Terminate,
+                        ])]),
+                        ..Default::default()
+                    },
+                ])
+            });
+
+        for recipe in recipe_list {
+            log::info!("[Recipe]: {}", &recipe);
+            recipe.run().await?;
+        }
+
+        println!("P2P pub/sub combination test passed.");
         Result::Ok(())
     })?;
     Ok(())
