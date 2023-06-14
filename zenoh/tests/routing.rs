1781755151700b4c3794178a6b7714c342b585df
yuanyuyuan <az6980522@gmail.com>
2023-06-15 00:05:42 +0800

    Extend the timeout again

--- zenoh/tests/routing.rs
+++ zenoh/tests/routing.rs
@@ -27,7 +27,7 @@ use zenoh_config::ModeDependentValue;
 use zenoh_core::zasync_executor_init;
 use zenoh_result::{bail, zerror};
 
-const TIMEOUT: Duration = Duration::from_secs(120);
+const TIMEOUT: Duration = Duration::from_secs(240);
 const MSG_COUNT: usize = 1_000;
 const MSG_SIZE: [usize; 2] = [1_024, 131_072];
 
