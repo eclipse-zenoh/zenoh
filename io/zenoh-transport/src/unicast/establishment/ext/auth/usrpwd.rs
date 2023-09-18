9922940791d0f14a48d8582515fda5e5add6a2f9
Luca Cominardi <luca.cominardi@gmail.com>
2023-09-18 12:07:58 +0200

    Move UsrPwd config test in dedicated test

--- io/zenoh-transport/src/unicast/establishment/ext/auth/usrpwd.rs
+++ io/zenoh-transport/src/unicast/establishment/ext/auth/usrpwd.rs
@@ -442,3 +442,62 @@ impl<'a> AcceptFsm for AuthUsrPwdFsm<'a> {
         Ok(Some(ZExtUnit::new()))
     }
 }
+
+mod tests {
+    #[test]
+    fn authenticator_usrpwd_config() {
+        async fn inner() {
+            use super::AuthUsrPwd;
+            use std::{fs::File, io::Write};
+            use zenoh_config::UsrPwdConf;
+
+            /* [CONFIG] */
+            let f1 = "zenoh-test-auth-usrpwd.txt";
+
+            let mut config = UsrPwdConf::default();
+            config.set_user(Some("usr1".to_owned())).unwrap();
+            config.set_password(Some("pwd1".to_owned())).unwrap();
+            config.set_dictionary_file(Some(f1.to_owned())).unwrap();
+
+            macro_rules! zconfig {
+                () => {
+                    File::options()
+                        .create(true)
+                        .write(true)
+                        .truncate(true)
+                        .open(f1)
+                        .unwrap()
+                };
+            }
+            // Valid config
+            let mut c = zconfig!();
+            writeln!(c, "usr1:pwd1").unwrap();
+            drop(c);
+            assert!(AuthUsrPwd::from_config(&config).await.unwrap().is_some());
+            // Invalid config
+            let mut c = zconfig!();
+            writeln!(c, "usr1").unwrap();
+            drop(c);
+            assert!(AuthUsrPwd::from_config(&config).await.is_err());
+            // Empty password
+            let mut c = zconfig!();
+            writeln!(c, "usr1:").unwrap();
+            drop(c);
+            assert!(AuthUsrPwd::from_config(&config).await.is_err());
+            // Empty user
+            let mut c = zconfig!();
+            writeln!(c, ":pwd1").unwrap();
+            drop(c);
+            assert!(AuthUsrPwd::from_config(&config).await.is_err());
+            // Empty user and password
+            let mut c = zconfig!();
+            writeln!(c, ":").unwrap();
+            drop(c);
+            assert!(AuthUsrPwd::from_config(&config).await.is_err());
+
+            let _ = std::fs::remove_file(f1);
+        }
+
+        async_std::task::block_on(inner());
+    }
+}
