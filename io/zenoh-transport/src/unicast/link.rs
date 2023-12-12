0fd53fe30c655bbf390fe86179bb2dcf74183538
Luca Cominardi <luca.cominardi@gmail.com>
2023-12-12 14:40:40 +0100

    Fix latency regression (#619)

--- io/zenoh-transport/src/unicast/link.rs
+++ io/zenoh-transport/src/unicast/link.rs
@@ -14,9 +14,8 @@
 use crate::common::batch::{BatchConfig, Decode, Encode, Finalize, RBatch, WBatch};
 use std::fmt;
 use std::sync::Arc;
-#[cfg(feature = "transport_compression")]
-use zenoh_buffers::BBuf;
-use zenoh_buffers::{ZSlice, ZSliceBuffer};
+use zenoh_buffers::{BBuf, ZSlice, ZSliceBuffer};
+use zenoh_core::zcondfeat;
 use zenoh_link::{Link, LinkUnicast};
 use zenoh_protocol::transport::{BatchSize, Close, TransportMessage};
 use zenoh_result::{zerror, ZResult};
@@ -31,11 +30,7 @@ pub(crate) enum TransportLinkUnicastDirection {
 pub(crate) struct TransportLinkUnicastConfig {
     // Inbound / outbound
     pub(crate) direction: TransportLinkUnicastDirection,
-    // MTU
-    pub(crate) mtu: BatchSize,
-    // Compression is active on the link
-    #[cfg(feature = "transport_compression")]
-    pub(crate) is_compression: bool,
+    pub(crate) batch: BatchConfig,
 }
 
 #[derive(Clone, PartialEq, Eq)]
@@ -46,25 +41,23 @@ pub(crate) struct TransportLinkUnicast {
 
 impl TransportLinkUnicast {
     pub(crate) fn new(link: LinkUnicast, mut config: TransportLinkUnicastConfig) -> Self {
-        config.mtu = link.get_mtu().min(config.mtu);
+        config.batch.mtu = link.get_mtu().min(config.batch.mtu);
         Self { link, config }
-    }
-
-    const fn batch_config(&self) -> BatchConfig {
-        BatchConfig {
-            mtu: self.config.mtu,
-            #[cfg(feature = "transport_compression")]
-            is_compression: self.config.is_compression,
-        }
     }
 
     pub(crate) fn tx(&self) -> TransportLinkUnicastTx {
         TransportLinkUnicastTx {
             inner: self.clone(),
-            #[cfg(feature = "transport_compression")]
-            buffer: self.config.is_compression.then_some(BBuf::with_capacity(
-                lz4_flex::block::get_maximum_output_size(self.config.mtu as usize),
-            )),
+            buffer: zcondfeat!(
+                "transport_compression",
+                self.config
+                    .batch
+                    .is_compression
+                    .then_some(BBuf::with_capacity(
+                        lz4_flex::block::get_maximum_output_size(self.config.batch.mtu as usize),
+                    )),
+                None
+            ),
         }
     }
 
@@ -128,7 +121,6 @@ impl From<TransportLinkUnicast> for Link {
 
 pub(crate) struct TransportLinkUnicastTx {
     pub(crate) inner: TransportLinkUnicast,
-    #[cfg(feature = "transport_compression")]
     pub(crate) buffer: Option<BBuf>,
 }
 
@@ -139,15 +131,11 @@ impl TransportLinkUnicastTx {
         // log::trace!("WBatch: {:?}", batch);
 
         let res = batch
-            .finalize(
-                #[cfg(feature = "transport_compression")]
-                self.buffer.as_mut(),
-            )
+            .finalize(self.buffer.as_mut())
             .map_err(|_| zerror!("{ERR}{self}"))?;
 
         let bytes = match res {
             Finalize::Batch => batch.as_slice(),
-            #[cfg(feature = "transport_compression")]
             Finalize::Buffer => self
                 .buffer
                 .as_ref()
@@ -158,14 +146,6 @@ impl TransportLinkUnicastTx {
         // log::trace!("WBytes: {:02x?}", bytes);
 
         // Send the message on the link
-        if self.inner.link.is_streamed() {
-            let len: BatchSize = bytes
-                .len()
-                .try_into()
-                .map_err(|_| zerror!("Invalid batch length"))?;
-            let len = len.to_le_bytes();
-            self.inner.link.write_all(&len).await?;
-        }
         self.inner.link.write_all(bytes).await?;
 
         Ok(())
@@ -175,7 +155,7 @@ impl TransportLinkUnicastTx {
         const ERR: &str = "Write error on link: ";
 
         // Create the batch for serializing the message
-        let mut batch = WBatch::new(self.inner.batch_config());
+        let mut batch = WBatch::new(self.inner.config.batch);
         batch.encode(msg).map_err(|_| zerror!("{ERR}{self}"))?;
         let len = batch.len() as usize;
         self.send_batch(&mut batch).await?;
@@ -191,14 +171,11 @@ impl fmt::Display for TransportLinkUnicastTx {
 
 impl fmt::Debug for TransportLinkUnicastTx {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let mut s = f.debug_struct("TransportLinkUnicastRx");
-        s.field("link", &self.inner.link)
-            .field("config", &self.inner.config);
-        #[cfg(feature = "transport_compression")]
-        {
-            s.field("buffer", &self.buffer.as_ref().map(|b| b.capacity()));
-        }
-        s.finish()
+        f.debug_struct("TransportLinkUnicastRx")
+            .field("link", &self.inner.link)
+            .field("config", &self.inner.config)
+            .field("buffer", &self.buffer.as_ref().map(|b| b.capacity()))
+            .finish()
     }
 }
 
@@ -219,15 +196,15 @@ impl TransportLinkUnicastRx {
             // Read and decode the message length
             let mut len = BatchSize::MIN.to_le_bytes();
             self.inner.link.read_exact(&mut len).await?;
-            let len = BatchSize::from_le_bytes(len) as usize;
+            let l = BatchSize::from_le_bytes(len) as usize;
 
             // Read the bytes
             let slice = into
                 .as_mut_slice()
-                .get_mut(..len)
+                .get_mut(len.len()..len.len() + l)
                 .ok_or_else(|| zerror!("{ERR}{self}. Invalid batch length or buffer size."))?;
             self.inner.link.read_exact(slice).await?;
-            len
+            len.len() + l
         } else {
             // Read the bytes
             self.inner.link.read(into.as_mut_slice()).await?
@@ -237,7 +214,7 @@ impl TransportLinkUnicastRx {
 
         let buffer = ZSlice::make(Arc::new(into), 0, end)
             .map_err(|_| zerror!("{ERR}{self}. ZSlice index(es) out of bounds"))?;
-        let mut batch = RBatch::new(self.inner.batch_config(), buffer);
+        let mut batch = RBatch::new(self.inner.config.batch, buffer);
         batch
             .initialize(buff)
             .map_err(|e| zerror!("{ERR}{self}. {e}."))?;
@@ -248,7 +225,7 @@ impl TransportLinkUnicastRx {
     }
 
     pub async fn recv(&mut self) -> ZResult<TransportMessage> {
-        let mtu = self.inner.config.mtu as usize;
+        let mtu = self.inner.config.batch.mtu as usize;
         let mut batch = self
             .recv_batch(|| zenoh_buffers::vec::uninit(mtu).into_boxed_slice())
             .await?;
