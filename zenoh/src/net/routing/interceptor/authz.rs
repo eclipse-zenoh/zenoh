c5579c9a15189d9964e23a0e9e98793e198eefce
snehilzs <snehil.joshi@zettascale.tech>
2024-01-26 22:04:01 +0100

    WIP:added interceptor code and PEP logic

--- zenoh/src/net/routing/interceptor/authz.rs
+++ zenoh/src/net/routing/interceptor/authz.rs
@@ -2,106 +2,238 @@ use std::fmt;
 
 // use casbin::{CoreApi, Enforcer};
 use super::RoutingContext;
-use casbin::prelude::*;
-use zenoh_keyexpr::keyexpr_tree::box_tree::KeBoxTree;
+use serde::{Deserialize, Serialize};
+use serde_json::{Result, Value};
+use zenoh_config::ZenohId;
+//use ZenohID;
+//use zenoh_keyexpr::keyexpr_tree::box_tree::KeBoxTree;
 use zenoh_protocol::network::NetworkMessage;
 use zenoh_result::ZResult;
 
+use std::{collections::HashMap, error::Error};
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
 pub enum Action {
-    NONE,
-    READ,
-    WRITE,
-    READWRITE,
-    SUBSCRIBEDECLARE,
+    Read,
+    Write,
+    Both,
 }
 
-impl fmt::Debug for Action {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{:?}", self)
+pub struct NewCtx {
+    pub(crate) ctx: RoutingContext<NetworkMessage>,
+    pub(crate) zid: Option<ZenohId>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Request {
+    sub: Subject,
+    obj: String,
+    action: Action,
+}
+
+pub struct RequestBuilder {
+    sub: Option<Subject>,
+    obj: Option<String>,
+    action: Option<Action>,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct Subject {
+    id: ZenohId,
+    attributes: Option<HashMap<String, String>>, //might be mapped to u8 values eventually
+}
+
+//subject_builder (add ID, attributes, roles)
+
+pub struct SubjectBuilder {
+    id: Option<ZenohId>,
+    attributes: Option<HashMap<String, String>>,
+}
+
+//request_builder (add subject, resource, action) //do we need one?
+
+impl RequestBuilder {
+    pub fn default() -> Self {
+        RequestBuilder {
+            sub: None,
+            obj: None,
+            action: None,
+        }
+    }
+    pub fn new() -> Self {
+        RequestBuilder::default()
+        //ctreas the default request
+    }
+
+    pub fn sub(&mut self, sub: impl Into<Subject>) -> &mut Self {
+        //adds subject
+        self.sub.insert(sub.into());
+        self
+    }
+
+    pub fn obj(&mut self, obj: impl Into<String>) -> &mut Self {
+        self.obj.insert(obj.into());
+        self
+    }
+
+    pub fn action(&mut self, action: impl Into<Action>) -> &mut Self {
+        self.action.insert(action.into());
+        self
+    }
+
+    pub fn build(&mut self) -> ZResult<Request> {
+        let Some(sub) = self.sub;
+        let Some(obj) = self.obj;
+        let Some(action) = self.action;
+
+        Ok(Request { sub, obj, action })
     }
 }
-impl fmt::Display for Action {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{:?}", self)
+
+impl SubjectBuilder {
+    pub fn new() -> Self {
+        //creates the default request
+        SubjectBuilder {
+            id: None,
+            attributes: None,
+        }
+    }
+
+    pub fn id(&mut self, id: impl Into<ZenohId>) -> &mut Self {
+        //adds subject
+        self.id.insert(id.into());
+        self
+    }
+
+    pub fn attributes(&mut self, attributes: impl Into<HashMap<String, String>>) -> &mut Self {
+        self.attributes.insert(attributes.into());
+        self
+    }
+
+    pub fn build(&mut self) -> ZResult<Subject> {
+        let Some(id) = self.id;
+        let attr = self.attributes;
+        Ok(Subject {
+            id,
+            attributes: attr,
+        })
     }
 }
+
+// impl fmt::Debug for Action {
+//     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+//         write!(f, "{:?}", self)
+//     }
+// }
+// impl fmt::Display for Action {
+//     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+//         write!(f, "{:?}", self)
+//     }
+// }
 
 pub trait ZAuth {
     fn authz_testing(&self, _: String, _: String, _: String) -> ZResult<bool>;
 }
 
-impl ZAuth for Enforcer {
-    fn authz_testing(&self, zid: String, ke: String, act: String) -> ZResult<bool> {
-        /*
-        (zid, keyexpr, act): these values should be extraced from the authn code.
-        has to be atomic, to avoid another process sending the wrong info
-         */
+// impl ZAuth for Enforcer {
+//     fn authz_testing(&self, zid: String, ke: String, act: String) -> ZResult<bool> {
+//         /*
+//         (zid, keyexpr, act): these values should be extraced from the authn code.
+//         has to be atomic, to avoid another process sending the wrong info
+//          */
+//         if let Ok(authorized) = self.enforce((zid.clone(), ke.clone(), act.clone())) {
+//             Ok(authorized)
+//         } else {
+//             println!("policy enforcement error");
+//             Ok(false)
+//         }
+//     }
+// }
 
-        if let Ok(authorized) = self.enforce((zid.clone(), ke.clone(), act.clone())) {
-            Ok(authorized)
-        } else {
-            println!("policy enforcement error");
-            Ok(false)
-        }
-    }
+/* replaced with PolicyEnforcer::init() function */
+
+// pub async fn start_authz() -> Result<Enforcer> {
+//     // get file value
+//     let mut e = Enforcer::new("keymatch_model.conf", "keymatch_policy.csv").await?;
+//     e.enable_log(true);
+//     Ok(e)
+// }
+
+//struct that defines each policy (add policy type and ruleset)
+#[derive(Serialize, Deserialize, Debug)]
+struct Policy {
+    // policy_type: u8, //for l,a,r [access-list, abac, rbac type policy] will be assuming acl for now
+    sub: Subject,
+    ke: String,
+    action: Action,
+    permission: bool,
 }
 
-pub async fn start_authz() -> Result<Enforcer> {
-    // get file value
-    let mut e = Enforcer::new("keymatch_model.conf", "keymatch_policy.csv").await?;
-    e.enable_log(true);
-    Ok(e)
-}
-
-struct StaticPolicy {
-    //should have values needed for the policy to work
-    //config for local static policy
-}
-
-//policy builder (add policy type and ruleset)
-
-//subject_builder (add ID, attributes, roles)
-
-//request_builder (add subject, resource, action) //do we need one?
-
-pub struct Subject {
-    //ID and list of possible policy attributes
-    id: Option<String>, //can convert uuid to string and back
-    attributes: Option<Vec<String>>,
-}
-
+#[derive(Clone)]
 pub struct PolicyEnforcer {
-    policy_config: String,
+    policy_config: HashMap<String, HashMap<String, String>>,
 }
 
 impl PolicyEnforcer {
-    pub fn init_policy(pol_string: String) -> ZResult<Self> {
-        let pe = Self {
-            policy_config: pol_string,
-        };
-        Ok(pe)
-    }
-    pub fn policy_enforcement_point(
-        &self,
-        ctx: RoutingContext<NetworkMessage>,
-        action: Action,
-    ) -> ZResult<bool> {
+    pub fn init() -> ZResult<Self> {
         /*
-           input: msg body
-           output: allow deny
-           function: depending on the msg, calls build_permission_request(msg,action), passes its output to policy_decision_point()
-                    uses allow/deny output to block or pass the msg to routing table
+           Initializs the policy for the control logic
+           loads policy into memory from file/network path
+           creates the policy hashmap with the ke-tries for ke matching
+           should have polic-type in the mix here...need to verify
         */
-        let ke = ctx.full_expr().unwrap().to_owned();
-        let zid = ctx.inface().unwrap().state.zid;
-        //build subject here
-        //for now just set it
-        let s = Subject {
-            id: Some(zid.to_string()),
-            attributes: None,
+        //policy should be derived from config/file (hardcoding it for now)
+        //config for local static policy
+        let static_policy = r#"{
+                            ["subject":{"id": "muyid", "attributes": "location"},"ke":"my_ke","action":"Read","permission":true],
+                            ["subject":{"id": "muyid", "attributes": "location"},"ke":"myke","action":"Read","permission":true],
+                            ["subject":{"id": "muyid", "attributes": "location"},"ke":"myke","action":"Read","permission":true],
+                            ["subject":{"id": "muyid", "attributes": "location"},"ke":"myke","action":"Read","permission":true]
+                        }"#;
+        //desearlize to policy
+        let get_policy: Policy = serde_json::from_str(static_policy)?;
+        println!("print policy {:?}", get_policy);
+        let policy = Self::build_policy_map(get_policy);
+
+        let pe = Self {
+            policy_config: policy,
         };
 
-        let decision = self.policy_decision_point(s, ke, action);
+        //also should start the logger here
+        Ok(pe)
+    }
+
+    pub fn build_policy_map(policy: Policy) {
+
+        //convert policy to vector of hashmap (WIP)
+        /*
+                       policy = subject : [ rule_1,
+                                           rule_2,
+                                           ...
+                                           rule_n
+                                        ]
+                       where rule_i = action_i : (ke_tree_deny, ke_tree_allow) that deny/allow action_i
+        */
+    }
+    pub fn policy_enforcement_point(&self, new_ctx: NewCtx, action: Action) -> ZResult<bool> {
+        /*
+           input: msg body
+           output: allow/deny
+           function: depending on the msg, builds the subject, builds the request, passes the request to policy_decision_point()
+                    collects result from PDP and then uses that allow/deny output to block or pass the msg to routing table
+        */
+
+        let Some(ke) = new_ctx.ctx.full_expr();
+        let zid = new_ctx.zid.unwrap();
+        //build subject here
+
+        let subject = SubjectBuilder::new().id(zid).build()?; //.attributes(None).build();
+        let request = RequestBuilder::new()
+            .sub(subject)
+            .obj(ke)
+            .action(action)
+            .build()?;
+        let decision = self.policy_decision_point(request)?;
         Ok(false)
     }
     pub fn permission_request_builder(
@@ -121,9 +253,9 @@ impl PolicyEnforcer {
            PHASE1: just extract the ID (zid?) from the msg; can later add attributes to the list. have a struct with ID and attributes field (both Option)
         */
     }
-    pub fn policy_decision_point(&self, subject: Subject, ke: String, action: u8) -> ZResult<bool> {
+    pub fn policy_decision_point(&self, request: Request) -> ZResult<bool> {
         /*
-            input: (sub,ke,act)
+            input: (request)
             output: true(allow)/false(deny)
             function: process the request from PEP against policy list
                     policy list will(might) be a hashmap of subject:rules_vector (test and discuss)
@@ -133,7 +265,10 @@ impl PolicyEnforcer {
         */
         //representaiton of policy list as a hashmap of trees?
         // HashMap<id,Hashmap<action,KeBoxTree>>
-        //use KeTrees for mapping R/W values? //need to check this out
+        /* use KeTrees for mapping R/W values? //need to check this out
+            tried KeTrees, didn't work
+            need own algorithm for pattern matching via modified trie-search
+        */
         Ok(false)
     }
 
@@ -150,4 +285,18 @@ impl PolicyEnforcer {
     }
 }
 
-pub fn testing_ke() {}
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn testing_acl_rules() {
+        //sample test stub
+        let result = 1 + 1;
+        assert_eq!(result, 2);
+    }
+    #[test]
+    fn testing_abac_rules() {
+        //sample test stub
+        let result = 1 + 1;
+        assert_eq!(result, 2);
+    }
+}
--- zenoh/src/net/routing/interceptor/mod.rs
+++ zenoh/src/net/routing/interceptor/mod.rs
@@ -17,9 +17,14 @@
 //! This module is intended for Zenoh's internal use.
 //!
 //! [Click here for Zenoh's documentation](../zenoh/index.html)
+//!
+mod authz;
+use self::authz::{Action, NewCtx};
+
 use super::RoutingContext;
-use zenoh_config::Config;
-use zenoh_protocol::network::NetworkMessage;
+use crate::net::routing::interceptor::authz::{PolicyEnforcer, ZAuth};
+use zenoh_config::{Config, ZenohId};
+use zenoh_protocol::network::{NetworkBody, NetworkMessage};
 use zenoh_transport::{multicast::TransportMulticast, unicast::TransportUnicast};
 
 pub(crate) trait InterceptorTrait {
@@ -48,7 +53,17 @@ pub(crate) fn interceptor_factories(_config: &Config) -> Vec<InterceptorFactory>
     // Add interceptors here
     // TODO build the list of intercetors with the correct order from the config
     // vec![Box::new(LoggerInterceptor {})]
-    vec![]
+    /*
+       this is the singleton for interceptors
+       all init code for AC should be called here
+       example, for casbin we are using the enforecer init here
+       for in-built AC, we will load the policy rules here and also set the parameters (type of policy etc)
+    */
+    println!("the interceptor is initialized");
+
+    let policy_enforcer = PolicyEnforcer::init().expect("error setting up access control");
+    //store the enforcer instance for use in rest of the sessions
+    vec![Box::new(AclEnforcer { e: policy_enforcer })]
 }
 
 pub(crate) struct InterceptorsChain {
@@ -140,5 +155,99 @@ impl InterceptorFactoryTrait for LoggerInterceptor {
     fn new_peer_multicast(&self, transport: &TransportMulticast) -> Option<IngressInterceptor> {
         log::debug!("New peer multicast {:?}", transport);
         Some(Box::new(IngressMsgLogger {}))
+    }
+}
+
+pub(crate) struct AclEnforcer {
+    e: PolicyEnforcer,
+}
+
+impl InterceptorFactoryTrait for AclEnforcer {
+    fn new_transport_unicast(
+        &self,
+        transport: &TransportUnicast,
+    ) -> (Option<IngressInterceptor>, Option<EgressInterceptor>) {
+        let e = self.e;
+
+        let uid = transport.get_zid().unwrap();
+        (
+            Some(Box::new(IngressAclEnforcer { e })),
+            Some(Box::new(EgressAclEnforcer { zid: Some(uid), e })),
+        )
+    }
+
+    fn new_transport_multicast(
+        &self,
+        _transport: &TransportMulticast,
+    ) -> Option<EgressInterceptor> {
+        let e = self.e;
+        //let uid = _transport.get_zid().unwrap();
+
+        Some(Box::new(EgressAclEnforcer { e, zid: None }))
+    }
+
+    fn new_peer_multicast(&self, _transport: &TransportMulticast) -> Option<IngressInterceptor> {
+        let e = self.e;
+        Some(Box::new(IngressAclEnforcer { e }))
+    }
+}
+
+pub(crate) struct IngressAclEnforcer {
+    //  e: Option<PolicyEnforcer>,
+    e: PolicyEnforcer,
+}
+
+impl InterceptorTrait for IngressAclEnforcer {
+    fn intercept(
+        &self,
+        ctx: RoutingContext<NetworkMessage>,
+    ) -> Option<RoutingContext<NetworkMessage>> {
+        //intercept msg and send it to PEP
+        if let NetworkBody::Push(push) = ctx.msg.body {
+            if let zenoh_protocol::zenoh::PushBody::Put(_put) = push.payload {
+                let e = self.e;
+                let act = Action::Write;
+                let new_ctx = NewCtx { ctx, zid: None };
+
+                let decision = e.policy_enforcement_point(new_ctx, act).unwrap();
+                if !decision {
+                    println!("Not allowed to Write");
+                    return None;
+                } else {
+                    println!("Allowed to Write");
+                }
+            }
+        }
+        Some(ctx)
+    }
+}
+
+pub(crate) struct EgressAclEnforcer {
+    e: PolicyEnforcer,
+    zid: Option<ZenohId>,
+}
+
+impl InterceptorTrait for EgressAclEnforcer {
+    fn intercept(
+        &self,
+        ctx: RoutingContext<NetworkMessage>,
+    ) -> Option<RoutingContext<NetworkMessage>> {
+        //intercept msg and send it to PEP
+        if let NetworkBody::Push(push) = ctx.msg.body {
+            if let zenoh_protocol::zenoh::PushBody::Put(_put) = push.payload {
+                let e = self.e;
+                let act = Action::Read;
+                let new_ctx = NewCtx { ctx, zid: self.zid };
+                let decision = e.policy_enforcement_point(new_ctx, act).unwrap();
+                if !decision {
+                    println!("Not allowed to Read");
+                    return None;
+                } else {
+                    println!("Allowed to Read");
+                }
+            }
+        }
+
+        Some(ctx)
     }
 }
