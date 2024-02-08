6221d695fba8a5b414a9101964ce4ee5a218efde
Alexander Bushnev <Alexander@Bushnev.pro>
2024-02-08 16:30:17 +0100

    Ignore result of get_interface_names_by_addr

--- io/zenoh-links/zenoh-link-tcp/src/unicast.rs
+++ io/zenoh-links/zenoh-link-tcp/src/unicast.rs
@@ -146,28 +146,28 @@ impl LinkUnicastTrait for LinkUnicastTcp {
 
     #[inline(always)]
     fn get_interface_names(&self) -> Vec<String> {
-        match zenoh_util::net::get_interface_names_by_addr(self.src_addr.ip()) {
-            Ok(interfaces) => {
-                /*
-                                log::trace!(
-                                    "get_interface_names for {:?}: {:?}",
-                                    self.src_addr.ip(),
-                                    interfaces
-                                );
-                */
-                interfaces
-            }
-            Err(e) => {
-                /*
-                                log::debug!(
-                                    "get_interface_names for {:?} failed: {:?}",
-                                    self.src_addr.ip(),
-                                    e
-                                );
-                */
-                vec![]
-            }
-        }
+        zenoh_util::net::get_interface_names_by_addr(self.src_addr.ip()).ok();
+        /*
+                match zenoh_util::net::get_interface_names_by_addr(self.src_addr.ip()) {
+                    Ok(interfaces) => {
+                        log::trace!(
+                            "get_interface_names for {:?}: {:?}",
+                            self.src_addr.ip(),
+                            interfaces
+                        );
+                        interfaces
+                    }
+                    Err(e) => {
+                        log::debug!(
+                            "get_interface_names for {:?} failed: {:?}",
+                            self.src_addr.ip(),
+                            e
+                        );
+                        vec![]
+                    }
+                }
+        */
+        vec![]
     }
 
     #[inline(always)]
