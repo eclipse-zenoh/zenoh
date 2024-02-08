8ed38aa311a52d920c67c9c64dc80212c573a604
Alexander Bushnev <Alexander@Bushnev.pro>
2024-02-08 16:55:05 +0100

    Revert and increase timeout

--- io/zenoh-links/zenoh-link-tcp/src/unicast.rs
+++ io/zenoh-links/zenoh-link-tcp/src/unicast.rs
@@ -146,28 +146,24 @@ impl LinkUnicastTrait for LinkUnicastTcp {
 
     #[inline(always)]
     fn get_interface_names(&self) -> Vec<String> {
-        zenoh_util::net::get_interface_names_by_addr(self.src_addr.ip()).ok();
-        /*
-                match zenoh_util::net::get_interface_names_by_addr(self.src_addr.ip()) {
-                    Ok(interfaces) => {
-                        log::trace!(
-                            "get_interface_names for {:?}: {:?}",
-                            self.src_addr.ip(),
-                            interfaces
-                        );
-                        interfaces
-                    }
-                    Err(e) => {
-                        log::debug!(
-                            "get_interface_names for {:?} failed: {:?}",
-                            self.src_addr.ip(),
-                            e
-                        );
-                        vec![]
-                    }
-                }
-        */
-        vec![]
+        match zenoh_util::net::get_interface_names_by_addr(self.src_addr.ip()) {
+            Ok(interfaces) => {
+                log::trace!(
+                    "get_interface_names for {:?}: {:?}",
+                    self.src_addr.ip(),
+                    interfaces
+                );
+                interfaces
+            }
+            Err(e) => {
+                log::debug!(
+                    "get_interface_names for {:?} failed: {:?}",
+                    self.src_addr.ip(),
+                    e
+                );
+                vec![]
+            }
+        }
     }
 
     #[inline(always)]
