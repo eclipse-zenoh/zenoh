3dc185c0d5e8247fc698a82cb327f663f5f1c783
Luca Cominardi <luca.cominardi@gmail.com>
2024-03-14 14:27:26 +0100

    Add express to examples

--- examples/examples/z_pub_thr.rs
+++ examples/examples/z_pub_thr.rs
@@ -41,7 +41,7 @@ fn main() {
         .declare_publisher("test/thr")
         .congestion_control(CongestionControl::Block)
         .priority(prio)
-        .express(true)
+        .express(args.express)
         .res()
         .unwrap();
 
@@ -65,6 +65,9 @@ fn main() {
 
 #[derive(Parser, Clone, PartialEq, Eq, Hash, Debug)]
 struct Args {
+    /// express for sending data
+    #[arg(long, default_value = "false")]
+    express: bool,
     /// Priority for sending data
     #[arg(short, long)]
     priority: Option<u8>,
