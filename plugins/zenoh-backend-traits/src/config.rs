5a1956495bb174a7addee12b117dda6c2c1f7184
Julien Loudet <julien.loudet@zettascale.tech>
2024-04-30 15:53:07 +0200

    feat(backend-trait): re-introduce Replication configuration
    This commit re-introduces the configuration that will be used to
    replicate a Storage.
    
    It is composed of the following fields:
    - `interval`: it is used both a way to group publications together and
      as a timer to publish the Replication information.
    - `sub_intervals`: it represents the number of sub-groups present inside
      an interval.
    - `hot`: it dictates the number of intervals that are considered recent
      and where differences are more likely -- eventually leading to sending
      more information to re-align them quickly.
    - `warm`: it dictates the number of intervals that are considered more
      stable than the "hot" intervals -- less information are sent.
    - `propagation_delay`: an estimated upper bound for the time it takes
      for a publication to reach a Zenoh node.
    
    * DEFAULT_CONFIG.json5: added, as an example, the default Replication
      configuration for a Storage.
    
    * plugins/zenoh-backend-traits/src/config.rs:
      - Introduced `interval` field to control the publication and the
        grouping of publications.
      - Introduced `sub_intervals` field to control the number of
        subdivisions within an `interval`.
      - Introduced `hot` field to control the size (i.e. number of
        intervals) of the hot era.
      - Introduced `warm` field to control the size (i.e. number of
        intervals) of the warm era.
      - Updated the `Default` trait implementation for the `ReplicaConfig`.
      - Parsed the `ReplicaConfig` from the Zenoh configuration.
    
    * plugins/zenoh-backend-traits/src/config.test.rs: unit tests to ensure
      that the parsing of the `ReplicaConfig` structure is valid.
    
    Signed-off-by: Julien Loudet <julien.loudet@zettascale.tech>

--- plugins/zenoh-backend-traits/src/config.rs
+++ plugins/zenoh-backend-traits/src/config.rs
@@ -65,6 +65,17 @@ pub struct StorageConfig {
     pub volume_id: String,
     pub volume_cfg: Value,
     pub garbage_collection_config: GarbageCollectionConfig,
+    // Note: ReplicaConfig is optional. Alignment will be performed only if it is a replica
+    pub replication: Option<ReplicaConfig>,
+}
+// Note: All parameters should be same for replicas, else will result on huge overhead
+#[derive(JsonSchema, Debug, Clone, PartialEq, Eq)]
+pub struct ReplicaConfig {
+    pub interval: Duration,
+    pub sub_intervals: usize,
+    pub hot: u64,
+    pub warm: u64,
+    pub propagation_delay: Duration,
 }
 
 impl StructVersion for VolumeConfig {
@@ -77,6 +88,67 @@ impl StructVersion for VolumeConfig {
 }
 
 impl PluginStartArgs for VolumeConfig {}
+
+impl Default for ReplicaConfig {
+    fn default() -> Self {
+        Self {
+            // This variable, expressed in SECONDS (f64), controls the frequency at which the
+            // Digests are computed and published.
+            //
+            // This also determines the time up to which replicas might diverge.
+            //
+            // Its default value is 10.0 seconds.
+            //
+            // ⚠️ THIS VALUE SHOULD BE THE SAME FOR ALL REPLICAS.
+            interval: Duration::from_secs_f64(10.0),
+            // This variable dictates the number of sub-intervals, of equal duration, within an
+            // interval.
+            //
+            // This is used to separate the publications in smaller batches when computing their
+            // fingerprints. A value of `1` will effectively disable the sub-intervals.
+            // Higher values will slightly increase the size of the `Digest` sent on the
+            // network but will reduce the amount of information sent when aligning.
+            //
+            // Hence, the trade-off is the following: with higher values more information are sent
+            // at every interval, with lower values more information are sent when
+            // aligning.
+            //
+            // Its default value is 5.
+            //
+            // ⚠️ THIS VALUE SHOULD BE THE SAME FOR ALL REPLICAS.
+            sub_intervals: 5,
+            // The number of intervals that compose the "hot" era.
+            //
+            // Its default value is 6 which, with the default `interval` value, corresponds to the
+            // last minute.
+            //
+            // ⚠️ THIS VALUE SHOULD BE THE SAME FOR ALL REPLICAS.
+            hot: 6,
+            // The number of intervals that compose the "warm" era.
+            //
+            // Its default value is 30 which, with the default `interval` value, corresponds to 5
+            // minutes.
+            //
+            // ⚠️ THIS VALUE SHOULD BE THE SAME FOR ALL REPLICAS.
+            warm: 30,
+            // The average time, expressed in MILLISECONDS, it takes for a publication to reach a
+            // storage.
+            //
+            // This value controls when the replication Digest is generated and, hence, published.
+            // Assuming that the `interval` is set to 10.0 seconds, with a
+            // `propagation_delay` of 250 milliseconds, the replication Digest
+            // will be computed at ( n × 10.0 ) + 0.25 seconds.
+            //
+            // ⚠️ For the reason above, this value cannot be greater or equal than half of the
+            //    duration of an `interval`.
+            //
+            // Its default value is 250 milliseconds.
+            //
+            // ⚠️ THIS VALUE SHOULD BE THE SAME FOR ALL REPLICAS.
+            propagation_delay: Duration::from_millis(250),
+        }
+    }
+}
 
 // The configuration for periodic garbage collection of metadata in storage manager
 #[derive(JsonSchema, Debug, Clone, PartialEq, Eq)]
@@ -475,6 +547,83 @@ impl StorageConfig {
             }
             None => GarbageCollectionConfig::default(),
         };
+        let replication = match config.get("replication") {
+            Some(s) => {
+                let mut replication = ReplicaConfig::default();
+                if let Some(p) = s.get("interval") {
+                    let p = p.to_string().parse::<f64>();
+                    if let Ok(p) = p {
+                        replication.interval = Duration::from_secs_f64(p);
+                    } else {
+                        bail!(
+                            "Invalid type for field `interval` in `replica_config` of storage \
+                             `{}`. Expecting integer or floating point number.",
+                            plugin_name
+                        )
+                    }
+                }
+                if let Some(p) = s.get("sub_intervals") {
+                    let p = p.to_string().parse::<usize>();
+                    if let Ok(p) = p {
+                        replication.sub_intervals = p;
+                    } else {
+                        bail!(
+                            "Invalid type for field `sub_intervals` in `replica_config` of \
+                             storage `{}`. Only integer values are accepted.",
+                            plugin_name
+                        )
+                    }
+                }
+                if let Some(d) = s.get("hot") {
+                    let d = d.to_string().parse::<u64>();
+                    if let Ok(d) = d {
+                        replication.hot = d;
+                    } else {
+                        bail!(
+                            "Invalid type for field `hot` in `replica_config` of storage `{}`. \
+                             Only integer values are accepted.",
+                            plugin_name
+                        )
+                    }
+                }
+                if let Some(d) = s.get("warm") {
+                    let d = d.to_string().parse::<u64>();
+                    if let Ok(d) = d {
+                        replication.warm = d;
+                    } else {
+                        bail!(
+                            "Invalid type for field `warm` in `replica_config` of storage `{}`. \
+                             Only integer values are accepted.",
+                            plugin_name
+                        )
+                    }
+                }
+                if let Some(p) = s.get("propagation_delay") {
+                    let p = p.to_string().parse::<u64>();
+                    if let Ok(p) = p {
+                        let propagation_delay = Duration::from_millis(p);
+                        if (replication.interval - propagation_delay) < propagation_delay {
+                            bail!(
+                                "Invalid value for field `propagation_delay`: its value is too \
+                                 high compared to the `interval`, consider increasing the \
+                                 `interval` to at least twice its value (i.e. {}).",
+                                p as f64 * 2.0 / 1000.0
+                            );
+                        }
+
+                        replication.propagation_delay = propagation_delay;
+                    } else {
+                        bail!(
+                            "Invalid type for field `propagation_delay` in `replica_config` of \
+                             storage `{}`. Only integer values are accepted.",
+                            plugin_name
+                        )
+                    }
+                }
+                Some(replication)
+            }
+            None => None,
+        };
         Ok(StorageConfig {
             name: storage_name.into(),
             key_expr,
@@ -483,6 +632,7 @@ impl StorageConfig {
             volume_id,
             volume_cfg,
             garbage_collection_config,
+            replication,
         })
     }
 }
@@ -518,3 +668,7 @@ impl PrivacyTransparentGet<serde_json::Value> for serde_json::Map<String, serde_
         }
     }
 }
+
+#[cfg(test)]
+#[path = "config.test.rs"]
+mod tests;
