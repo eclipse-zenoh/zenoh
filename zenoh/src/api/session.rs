8dde1d1999a7a549b59fb2d68400eaf58a5a66ff
OlivierHecart <olivier.hecart@adlinktech.com>
2024-06-06 10:46:25 +0200

    Fix clippy warnings

--- zenoh/src/api/session.rs
+++ zenoh/src/api/session.rs
@@ -2396,7 +2396,7 @@ impl Primitives for Session {
                     encoding: Some(m.encoding.into()),
                     timestamp: m.timestamp,
                     qos: QoS::from(msg.ext_qos),
-                    source_id: m.ext_sinfo.as_ref().map(|i| i.id.clone()),
+                    source_id: m.ext_sinfo.as_ref().map(|i| i.id),
                     source_sn: m.ext_sinfo.as_ref().map(|i| i.sn as u64),
                 };
                 self.execute_subscriber_callbacks(
@@ -2415,7 +2415,7 @@ impl Primitives for Session {
                     encoding: None,
                     timestamp: m.timestamp,
                     qos: QoS::from(msg.ext_qos),
-                    source_id: m.ext_sinfo.as_ref().map(|i| i.id.clone()),
+                    source_id: m.ext_sinfo.as_ref().map(|i| i.id),
                     source_sn: m.ext_sinfo.as_ref().map(|i| i.sn as u64),
                 };
                 self.execute_subscriber_callbacks(
@@ -2557,7 +2557,7 @@ impl Primitives for Session {
                                     encoding: Some(encoding.into()),
                                     timestamp,
                                     qos: QoS::from(msg.ext_qos),
-                                    source_id: ext_sinfo.as_ref().map(|i| i.id.clone()),
+                                    source_id: ext_sinfo.as_ref().map(|i| i.id),
                                     source_sn: ext_sinfo.as_ref().map(|i| i.sn as u64),
                                 },
                                 #[cfg(feature = "unstable")]
@@ -2575,7 +2575,7 @@ impl Primitives for Session {
                                     encoding: None,
                                     timestamp,
                                     qos: QoS::from(msg.ext_qos),
-                                    source_id: ext_sinfo.as_ref().map(|i| i.id.clone()),
+                                    source_id: ext_sinfo.as_ref().map(|i| i.id),
                                     source_sn: ext_sinfo.as_ref().map(|i| i.sn as u64),
                                 },
                                 #[cfg(feature = "unstable")]
