5bce0ea5a4e99a0f07fe198e755322dd26393cca
Luca Cominardi <luca.cominardi@gmail.com>
2024-06-19 11:44:51 +0200

    Disable downsampling_by_keyexpr on windows (#1170)

--- zenoh/tests/interceptors.rs
+++ zenoh/tests/interceptors.rs
@@ -180,6 +180,7 @@ fn downsampling_by_keyexpr_impl(flow: InterceptorFlow) {
     downsampling_test(pub_config, sub_config, ke_prefix, ke_of_rates, rate_check);
 }
 
+#[cfg(unix)]
 #[test]
 fn downsampling_by_keyexpr() {
     zenoh::try_init_log_from_env();
