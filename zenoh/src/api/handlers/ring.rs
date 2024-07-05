c7e418f526277c3aa8f7810a006264208659772e
J-Loudet <julien.loudet@zettascale.tech>
2024-07-05 11:22:26 +0200

    fix: typos (#1220)
    * zenoh/src/api/handlers/ring.rs: synchrounous -> synchronous
    * zenoh/src/api/selector.rs: intendend -> intended
    
    Signed-off-by: Julien Loudet <julien.loudet@zettascale.tech>

--- zenoh/src/api/handlers/ring.rs
+++ zenoh/src/api/handlers/ring.rs
@@ -24,7 +24,7 @@ use zenoh_result::ZResult;
 use super::{callback::Callback, Dyn, IntoHandler};
 use crate::api::session::API_DATA_RECEPTION_CHANNEL_SIZE;
 
-/// A synchrounous ring channel with a limited size that allows users to keep the last N data.
+/// A synchronous ring channel with a limited size that allows users to keep the last N data.
 pub struct RingChannel {
     capacity: usize,
 }
