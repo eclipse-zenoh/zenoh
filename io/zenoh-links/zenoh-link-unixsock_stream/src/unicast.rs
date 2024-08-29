79652a72d9ada98f59d1acc9df4846b3c89076c3
Luca Cominardi <luca.cominardi@gmail.com>
2024-08-29 10:18:33 +0200

    Fix dependencies

--- io/zenoh-links/zenoh-link-unixsock_stream/src/unicast.rs
+++ io/zenoh-links/zenoh-link-unixsock_stream/src/unicast.rs
@@ -328,6 +328,8 @@ impl LinkManagerUnicastTrait for LinkManagerUnicastUnixSocketStream {
         })?;
 
         // We try to acquire the lock
+        // @TODO: updating the deprecated function will require the refactoring of this module
+        #[allow(deprecated)]
         nix::fcntl::flock(lock_fd, nix::fcntl::FlockArg::LockExclusiveNonblock).map_err(|e| {
             let _ = nix::unistd::close(lock_fd);
             let e = zerror!(
@@ -427,6 +429,8 @@ impl LinkManagerUnicastTrait for LinkManagerUnicastUnixSocketStream {
         listener.handle.await??;
 
         //Release the lock
+        // @TODO: updating the deprecated function will require the refactoring of this module
+        #[allow(deprecated)]
         let _ = nix::fcntl::flock(listener.lock_fd, nix::fcntl::FlockArg::UnlockNonblock);
         let _ = nix::unistd::close(listener.lock_fd);
         let _ = remove_file(path.clone());
