f595fed764bcedc906a3e580ea9f822b9ea20a31
Luca Cominardi <luca.cominardi@gmail.com>
2024-09-06 12:33:16 +0200

    Update zenoh/src/net/routing/hat/linkstate_peer/queries.rs
    Co-authored-by: Joseph Perez <joperez@hotmail.fr>

--- zenoh/src/net/routing/hat/linkstate_peer/queries.rs
+++ zenoh/src/net/routing/hat/linkstate_peer/queries.rs
@@ -1006,7 +1006,7 @@ impl HatQueriesTrait for HatCode {
                 }
             }
         }
-        route.sort_by_key(|qabl| qabl.info.map(|i| i.distance).unwrap_or(u16::MAX));
+        route.sort_by_key(|qabl| qabl.info.map_or(u16::MAX, |i| i.distance));
         Arc::new(route)
     }
 
