b589b78fa59368985fa1ece76b5cc5ceec19d7b8
Joseph Perez <joperez@hotmail.fr>
2024-09-25 18:35:24 +0200

    refactor: rename write_xxx/read_xxx to (de)serialize_xxx

--- zenoh-ext/src/zbytes_ext.rs
+++ zenoh-ext/src/zbytes_ext.rs
@@ -20,26 +20,26 @@ impl fmt::Display for ZDeserializeError {
 impl std::error::Error for ZDeserializeError {}
 
 pub trait Serialize {
-    fn write_into(&self, writer: &mut ZBytesWriter);
+    fn serialize(&self, writer: &mut ZBytesWriter);
     #[doc(hidden)]
-    fn write_slice_into(slice: &[Self], writer: &mut ZBytesWriter)
+    fn serialize_slice(slice: &[Self], writer: &mut ZBytesWriter)
     where
         Self: Sized,
     {
-        writer.write_iter(slice)
+        writer.serialize_iter(slice)
     }
 }
 impl<T: Serialize> Serialize for &T {
-    fn write_into(&self, writer: &mut ZBytesWriter) {
-        T::write_into(*self, writer)
+    fn serialize(&self, writer: &mut ZBytesWriter) {
+        T::serialize(*self, writer)
     }
 }
 
 pub trait Deserialize: Sized {
-    fn read_from(reader: &mut ZBytesReader) -> Result<Self, ZDeserializeError>;
+    fn deserialize(reader: &mut ZBytesReader) -> Result<Self, ZDeserializeError>;
     #[doc(hidden)]
-    fn read_slice_from(reader: &mut ZBytesReader) -> Result<Box<[Self]>, ZDeserializeError> {
-        reader.read_iter::<Self>()?.collect()
+    fn deserialize_slice(reader: &mut ZBytesReader) -> Result<Box<[Self]>, ZDeserializeError> {
+        reader.deserialize_iter::<Self>()?.collect()
     }
 }
 
@@ -51,13 +51,13 @@ pub trait ZBytesExt {
 impl ZBytesExt for ZBytes {
     fn serialize<T: Serialize + ?Sized>(t: &T) -> Self {
         let mut zbytes = ZBytes::new();
-        t.write_into(&mut zbytes.writer());
+        t.serialize(&mut zbytes.writer());
         zbytes
     }
 
     fn deserialize<T: Deserialize>(&self) -> Result<T, ZDeserializeError> {
         let mut reader = self.reader();
-        let t = T::read_from(&mut reader)?;
+        let t = T::deserialize(&mut reader)?;
         if !reader.is_empty() {
             return Err(ZDeserializeError);
         }
@@ -66,29 +66,43 @@ impl ZBytesExt for ZBytes {
 }
 
 pub trait ZBytesWriterExt<'a> {
-    fn write_iter<T: Serialize, I: IntoIterator<Item = T>>(&mut self, iter: I)
+    fn serialize<T: Serialize>(&mut self, t: T);
+    fn serialize_iter<T: Serialize, I: IntoIterator<Item = T>>(&mut self, iter: I)
     where
         I::IntoIter: ExactSizeIterator;
 }
 
 impl<'a> ZBytesWriterExt<'a> for ZBytesWriter<'a> {
-    fn write_iter<T: Serialize, I: IntoIterator<Item = T>>(&mut self, iter: I)
+    fn serialize<T: Serialize>(&mut self, t: T) {
+        t.serialize(self);
+    }
+
+    fn serialize_iter<T: Serialize, I: IntoIterator<Item = T>>(&mut self, iter: I)
     where
         I::IntoIter: ExactSizeIterator,
     {
         let iter = iter.into_iter();
         self.write_vle(iter.len() as u64);
         for t in iter {
-            t.write_into(self)
+            t.serialize(self);
         }
     }
 }
 
 pub trait ZBytesReaderExt<'a> {
-    fn read_iter<T: Deserialize>(&mut self) -> Result<ZReadIter<'a, '_, T>, ZDeserializeError>;
+    fn deserialize<T: Deserialize>(&mut self) -> Result<T, ZDeserializeError>;
+    fn deserialize_iter<T: Deserialize>(
+        &mut self,
+    ) -> Result<ZReadIter<'a, '_, T>, ZDeserializeError>;
 }
 impl<'a> ZBytesReaderExt<'a> for ZBytesReader<'a> {
-    fn read_iter<T: Deserialize>(&mut self) -> Result<ZReadIter<'a, '_, T>, ZDeserializeError> {
+    fn deserialize<T: Deserialize>(&mut self) -> Result<T, ZDeserializeError> {
+        T::deserialize(self)
+    }
+
+    fn deserialize_iter<T: Deserialize>(
+        &mut self,
+    ) -> Result<ZReadIter<'a, '_, T>, ZDeserializeError> {
         let len = self.read_vle().ok_or(ZDeserializeError)? as usize;
         Ok(ZReadIter {
             reader: self,
@@ -110,7 +124,7 @@ impl<T: Deserialize> Iterator for ZReadIter<'_, '_, T> {
             return None;
         }
         self.len -= 1;
-        Some(T::read_from(self.reader))
+        Some(T::deserialize(self.reader))
     }
 
     fn size_hint(&self) -> (usize, Option<usize>) {
@@ -127,26 +141,26 @@ impl<T: Deserialize> Drop for ZReadIter<'_, '_, T> {
 macro_rules! impl_num {
     ($($ty:ty),* $(,)?) => {$(
         impl Serialize for $ty {
-            fn write_into(&self, writer: &mut ZBytesWriter) {
+            fn serialize(&self, writer: &mut ZBytesWriter) {
                 writer.write(&(*self).to_le_bytes()).unwrap();
             }
-            fn write_slice_into(slice: &[Self], writer: &mut ZBytesWriter) where Self: Sized {
+            fn serialize_slice(slice: &[Self], writer: &mut ZBytesWriter) where Self: Sized {
                 if cfg!(target_endian = "little") || std::mem::size_of::<Self>() == 1 {
                     writer.write_vle(slice.len() as u64);
                     // SAFETY: transmuting numeric types to their little endian bytes is safe
                     writer.write(unsafe { slice.align_to().1 }).unwrap();
                 } else {
-                    writer.write_iter(slice);
+                    writer.serialize_iter(slice);
                 }
             }
         }
         impl Deserialize for $ty {
-            fn read_from(reader: &mut ZBytesReader) -> Result<Self, ZDeserializeError> {
+            fn deserialize(reader: &mut ZBytesReader) -> Result<Self, ZDeserializeError> {
                 let mut buf = [0; { std::mem::size_of::<Self>() }];
                 reader.read(&mut buf).or(Err(ZDeserializeError))?;
                 Ok(<$ty>::from_le_bytes(buf))
             }
-            fn read_slice_from(reader: &mut ZBytesReader) -> Result<Box<[Self]>, ZDeserializeError> {
+            fn deserialize_slice(reader: &mut ZBytesReader) -> Result<Box<[Self]>, ZDeserializeError> {
                 let size = std::mem::size_of::<Self>();
                 if cfg!(target_endian = "little") || size == 1 {
                     let len = reader.read_vle().ok_or(ZDeserializeError)? as usize;
@@ -156,7 +170,7 @@ macro_rules! impl_num {
                     // SAFETY: transmuting numeric types from their little endian bytes is safe
                     Ok(unsafe { Box::from_raw(ptr::slice_from_raw_parts_mut(buf.as_mut_ptr().cast(), len)) })
                 } else {
-                    reader.read_iter::<Self>()?.collect()
+                    reader.deserialize_iter::<Self>()?.collect()
                 }
             }
         }
@@ -165,13 +179,13 @@ macro_rules! impl_num {
 impl_num!(i8, i16, i32, i64, i128, u8, u16, u32, u64, u128, f32, f64);
 
 impl Serialize for bool {
-    fn write_into(&self, writer: &mut ZBytesWriter) {
-        (*self as u8).write_into(writer);
+    fn serialize(&self, writer: &mut ZBytesWriter) {
+        (*self as u8).serialize(writer);
     }
 }
 impl Deserialize for bool {
-    fn read_from(reader: &mut ZBytesReader) -> Result<Self, ZDeserializeError> {
-        match u8::read_from(reader)? {
+    fn deserialize(reader: &mut ZBytesReader) -> Result<Self, ZDeserializeError> {
+        match u8::deserialize(reader)? {
             0 => Ok(false),
             1 => Ok(true),
             _ => Err(ZDeserializeError),
@@ -180,96 +194,96 @@ impl Deserialize for bool {
 }
 
 impl<T: Serialize> Serialize for [T] {
-    fn write_into(&self, writer: &mut ZBytesWriter) {
-        T::write_slice_into(self, writer)
+    fn serialize(&self, writer: &mut ZBytesWriter) {
+        T::serialize_slice(self, writer)
     }
 }
 impl<'a, T: Serialize + 'a> Serialize for Cow<'a, [T]>
 where
     [T]: ToOwned,
 {
-    fn write_into(&self, writer: &mut ZBytesWriter) {
-        T::write_slice_into(self, writer)
+    fn serialize(&self, writer: &mut ZBytesWriter) {
+        T::serialize_slice(self, writer)
     }
 }
 impl<T: Serialize> Serialize for Box<[T]> {
-    fn write_into(&self, writer: &mut ZBytesWriter) {
-        T::write_slice_into(self, writer)
+    fn serialize(&self, writer: &mut ZBytesWriter) {
+        T::serialize_slice(self, writer)
     }
 }
 impl<T: Deserialize> Deserialize for Box<[T]> {
-    fn read_from(reader: &mut ZBytesReader) -> Result<Self, ZDeserializeError> {
-        T::read_slice_from(reader)
+    fn deserialize(reader: &mut ZBytesReader) -> Result<Self, ZDeserializeError> {
+        T::deserialize_slice(reader)
     }
 }
 impl<T: Serialize> Serialize for Vec<T> {
-    fn write_into(&self, writer: &mut ZBytesWriter) {
-        T::write_slice_into(self, writer)
+    fn serialize(&self, writer: &mut ZBytesWriter) {
+        T::serialize_slice(self, writer)
     }
 }
 impl<T: Deserialize> Deserialize for Vec<T> {
-    fn read_from(reader: &mut ZBytesReader) -> Result<Self, ZDeserializeError> {
-        T::read_slice_from(reader).map(Into::into)
+    fn deserialize(reader: &mut ZBytesReader) -> Result<Self, ZDeserializeError> {
+        T::deserialize_slice(reader).map(Into::into)
     }
 }
 impl<T: Serialize + Eq + Hash> Serialize for HashSet<T> {
-    fn write_into(&self, writer: &mut ZBytesWriter) {
-        writer.write_iter(self);
+    fn serialize(&self, writer: &mut ZBytesWriter) {
+        writer.serialize_iter(self);
     }
 }
 impl<T: Deserialize + Eq + Hash> Deserialize for HashSet<T> {
-    fn read_from(reader: &mut ZBytesReader) -> Result<Self, ZDeserializeError> {
-        reader.read_iter::<T>()?.collect()
+    fn deserialize(reader: &mut ZBytesReader) -> Result<Self, ZDeserializeError> {
+        reader.deserialize_iter::<T>()?.collect()
     }
 }
 impl<T: Serialize + Ord> Serialize for BTreeSet<T> {
-    fn write_into(&self, writer: &mut ZBytesWriter) {
-        writer.write_iter(self);
+    fn serialize(&self, writer: &mut ZBytesWriter) {
+        writer.serialize_iter(self);
     }
 }
 impl<T: Deserialize + Ord> Deserialize for BTreeSet<T> {
-    fn read_from(reader: &mut ZBytesReader) -> Result<Self, ZDeserializeError> {
-        reader.read_iter::<T>()?.collect()
+    fn deserialize(reader: &mut ZBytesReader) -> Result<Self, ZDeserializeError> {
+        reader.deserialize_iter::<T>()?.collect()
     }
 }
 impl<K: Serialize + Eq + Hash, V: Serialize> Serialize for HashMap<K, V> {
-    fn write_into(&self, writer: &mut ZBytesWriter) {
-        writer.write_iter(self);
+    fn serialize(&self, writer: &mut ZBytesWriter) {
+        writer.serialize_iter(self);
     }
 }
 impl<K: Deserialize + Eq + Hash, V: Deserialize> Deserialize for HashMap<K, V> {
-    fn read_from(reader: &mut ZBytesReader) -> Result<Self, ZDeserializeError> {
-        reader.read_iter::<(K, V)>()?.collect()
+    fn deserialize(reader: &mut ZBytesReader) -> Result<Self, ZDeserializeError> {
+        reader.deserialize_iter::<(K, V)>()?.collect()
     }
 }
 impl<K: Serialize + Ord, V: Serialize> Serialize for BTreeMap<K, V> {
-    fn write_into(&self, writer: &mut ZBytesWriter) {
-        writer.write_iter(self);
+    fn serialize(&self, writer: &mut ZBytesWriter) {
+        writer.serialize_iter(self);
     }
 }
 impl<K: Deserialize + Ord, V: Deserialize> Deserialize for BTreeMap<K, V> {
-    fn read_from(reader: &mut ZBytesReader) -> Result<Self, ZDeserializeError> {
-        reader.read_iter::<(K, V)>()?.collect()
+    fn deserialize(reader: &mut ZBytesReader) -> Result<Self, ZDeserializeError> {
+        reader.deserialize_iter::<(K, V)>()?.collect()
     }
 }
 impl Serialize for str {
-    fn write_into(&self, writer: &mut ZBytesWriter) {
-        self.as_bytes().write_into(writer);
+    fn serialize(&self, writer: &mut ZBytesWriter) {
+        self.as_bytes().serialize(writer);
     }
 }
 impl Serialize for Cow<'_, str> {
-    fn write_into(&self, writer: &mut ZBytesWriter) {
-        self.as_bytes().write_into(writer);
+    fn serialize(&self, writer: &mut ZBytesWriter) {
+        self.as_bytes().serialize(writer);
     }
 }
 impl Serialize for String {
-    fn write_into(&self, writer: &mut ZBytesWriter) {
-        self.as_bytes().write_into(writer);
+    fn serialize(&self, writer: &mut ZBytesWriter) {
+        self.as_bytes().serialize(writer);
     }
 }
 impl Deserialize for String {
-    fn read_from(reader: &mut ZBytesReader) -> Result<Self, ZDeserializeError> {
-        String::from_utf8(Deserialize::read_from(reader)?).or(Err(ZDeserializeError))
+    fn deserialize(reader: &mut ZBytesReader) -> Result<Self, ZDeserializeError> {
+        String::from_utf8(Deserialize::deserialize(reader)?).or(Err(ZDeserializeError))
     }
 }
 
@@ -287,14 +301,14 @@ macro_rules! impl_tuple {
     (@@$($ty:ident/$i:tt),* $(,)?) => {
         #[allow(unused)]
         impl<$($ty: Serialize),*> Serialize for ($($ty,)*) {
-            fn write_into(&self, writer: &mut ZBytesWriter) {
-                $(self.$i.write_into(writer);)*
+            fn serialize(&self, writer: &mut ZBytesWriter) {
+                $(self.$i.serialize(writer);)*
             }
         }
         #[allow(unused)]
         impl<$($ty: Deserialize),*> Deserialize for ($($ty,)*) {
-            fn read_from(reader: &mut ZBytesReader) -> Result<Self, ZDeserializeError> {
-                Ok(($($ty::read_from(reader)?,)*))
+            fn deserialize(reader: &mut ZBytesReader) -> Result<Self, ZDeserializeError> {
+                Ok(($($ty::deserialize(reader)?,)*))
             }
         }
     };
