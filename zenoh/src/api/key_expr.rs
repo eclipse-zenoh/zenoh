c54ed52a892c01521b36c6bbee2a820f523fde1c
Denis Biryukov <denis.biryukov@zettascale.tech>
2024-11-25 15:55:23 +0100

    log error when keyexpr_intersect/includes fails keyexpr conversion

--- zenoh/src/api/key_expr.rs
+++ zenoh/src/api/key_expr.rs
@@ -273,32 +273,54 @@ impl<'a> KeyExpr<'a> {
         }
     }
 
-    /// Will return false in case of TryInto failure.
+    /// Will return false and log a error in case of TryInto failure.
     #[inline]
     pub(crate) fn keyexpr_intersect<'b, L, R>(left: L, right: R) -> bool
     where
         L: TryInto<KeyExpr<'a>>,
         R: TryInto<KeyExpr<'b>>,
+        L::Error: std::fmt::Display,
+        R::Error: std::fmt::Display,
     {
-        if let (Ok(l), Ok(r)) = (left.try_into(), right.try_into()) {
-            l.intersects(&r)
-        } else {
-            false
+        match left.try_into() {
+            Ok(l) => match right.try_into() {
+                Ok(r) => {
+                    return l.intersects(&r);
+                }
+                Err(e) => {
+                    tracing::error!("{e}");
+                }
+            },
+            Err(e) => {
+                tracing::error!("{e}");
+            }
         }
+        false
     }
 
-    /// Will return false in case of TryInto failure.
+    /// Will return false and log a error in case of TryInto failure.
     #[inline]
     pub(crate) fn keyexpr_include<'b, L, R>(left: L, right: R) -> bool
     where
         L: TryInto<KeyExpr<'a>>,
         R: TryInto<KeyExpr<'b>>,
+        L::Error: std::fmt::Display,
+        R::Error: std::fmt::Display,
     {
-        if let (Ok(l), Ok(r)) = (left.try_into(), right.try_into()) {
-            l.includes(&r)
-        } else {
-            false
+        match left.try_into() {
+            Ok(l) => match right.try_into() {
+                Ok(r) => {
+                    return l.includes(&r);
+                }
+                Err(e) => {
+                    tracing::error!("{e}");
+                }
+            },
+            Err(e) => {
+                tracing::error!("{e}");
+            }
         }
+        false
     }
 }
 
