c8083f729156d20cde342b96b559abd8fcc603f5
Oussama Teffahi <oussama.teffahi@zettascale.tech>
2024-11-26 17:51:41 +0100

    Add publisher builder overwrite test

--- zenoh/tests/qos.rs
+++ zenoh/tests/qos.rs
@@ -62,3 +62,67 @@ async fn qos_pubsub() {
     assert_eq!(sample.congestion_control(), CongestionControl::Block);
     assert!(!sample.express());
 }
+
+#[tokio::test(flavor = "multi_thread", worker_threads = 4)]
+async fn qos_pubsub_overwrite_builder() {
+    let builder_config_overwrite = zenoh::Config::from_json5(
+        r#"
+        {
+            builders: {
+                publishers: [
+                    {
+                        key_exprs: ["test/qos/overwritten", "test/not_applicable/**"],
+                        priority: "real_time",
+                        encoding: "zenoh/string",
+                        congestion_control: "drop",
+                        express: false,
+                    },
+                    {
+                        key_exprs: ["test/not_applicable"],
+                        priority: "data_high",
+                        encoding: "zenoh/bytes",
+                        congestion_control: "drop",
+                        express: false,
+                    },
+                ]
+            }
+        }
+    "#,
+    )
+    .unwrap();
+    let session1 = ztimeout!(zenoh::open(builder_config_overwrite)).unwrap();
+    let session2 = ztimeout!(zenoh::open(zenoh::Config::default())).unwrap();
+
+    let overwrite_config_publisher = ztimeout!(session1
+        .declare_publisher("test/qos/overwritten")
+        .congestion_control(CongestionControl::Block)
+        .express(true))
+    .unwrap();
+
+    let no_overwrite_config_publisher = ztimeout!(session1
+        .declare_publisher("test/qos/no_overwrite")
+        .encoding(Encoding::TEXT_PLAIN)
+        .priority(Priority::DataLow)
+        .congestion_control(CongestionControl::Drop)
+        .express(false))
+    .unwrap();
+
+    let subscriber = ztimeout!(session2.declare_subscriber("test/qos/**")).unwrap();
+    tokio::time::sleep(SLEEP).await;
+
+    ztimeout!(overwrite_config_publisher.put("qos")).unwrap();
+    let sample = ztimeout!(subscriber.recv_async()).unwrap();
+
+    assert_eq!(sample.priority(), Priority::RealTime);
+    assert_eq!(sample.encoding(), &Encoding::ZENOH_STRING);
+    assert_eq!(sample.congestion_control(), CongestionControl::Block);
+    assert!(sample.express());
+
+    ztimeout!(no_overwrite_config_publisher.put("qos")).unwrap();
+    let sample = ztimeout!(subscriber.recv_async()).unwrap();
+
+    assert_eq!(sample.encoding(), &Encoding::TEXT_PLAIN);
+    assert_eq!(sample.priority(), Priority::DataLow);
+    assert_eq!(sample.congestion_control(), CongestionControl::Drop);
+    assert!(!sample.express());
+}
