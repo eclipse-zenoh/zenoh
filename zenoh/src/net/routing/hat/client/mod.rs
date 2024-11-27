6c7609658e5936170d1f0e3dfe1c67f60f62b6d8
OlivierHecart <olivier.hecart@adlinktech.com>
2024-11-27 23:52:24 +0100

    Forbid routing from/to multicast groups

--- zenoh/src/net/routing/hat/client/mod.rs
+++ zenoh/src/net/routing/hat/client/mod.rs
@@ -275,10 +275,8 @@ impl HatBaseTrait for HatCode {
         _expr: &mut RoutingExpr,
     ) -> bool {
         src_face.id != out_face.id
-            && match (src_face.mcast_group.as_ref(), out_face.mcast_group.as_ref()) {
-                (Some(l), Some(r)) => l != r,
-                _ => true,
-            }
+            && (out_face.mcast_group.is_none()
+                || (src_face.whatami == WhatAmI::Client && src_face.mcast_group.is_none()))
     }
 
     fn info(&self, _tables: &Tables, _kind: WhatAmI) -> String {
