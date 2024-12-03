1d6af814eae920a493c0a80d7bac67f60c8cc056
Oussama Teffahi <oussama.teffahi@zettascale.tech>
2024-12-03 11:57:53 +0100

    Remove unicity check of keyexpr in qos config

--- commons/zenoh-config/src/qos.rs
+++ commons/zenoh-config/src/qos.rs
@@ -1,50 +1,16 @@
-use std::collections::HashSet;
-
-use serde::{Deserialize, Deserializer, Serialize};
+use serde::{Deserialize, Serialize};
 use zenoh_keyexpr::keyexpr_tree::{IKeyExprTreeMut, KeBoxTree};
 use zenoh_protocol::core::{key_expr::OwnedKeyExpr, CongestionControl, Reliability};
 
 #[derive(Debug, Deserialize, Default, Serialize, Clone)]
-#[serde(remote = "Self")]
 pub struct PublisherQoSConfList(pub(crate) Vec<PublisherQoSConf>);
-
-impl<'de> Deserialize<'de> for PublisherQoSConfList {
-    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-    where
-        D: Deserializer<'de>,
-    {
-        let put_qos_list = PublisherQoSConfList::deserialize(deserializer)?;
-        // check for invariant: each key_expr should be unique
-        let mut key_set = HashSet::new();
-        for put_qos in &put_qos_list.0 {
-            for key_expr in &put_qos.key_exprs {
-                if !key_set.insert(key_expr) {
-                    return Err(format!(
-                        "duplicated key_expr '{key_expr}' found in publisher builders config"
-                    ))
-                    .map_err(serde::de::Error::custom);
-                }
-            }
-        }
-        Ok(put_qos_list)
-    }
-}
-
-impl Serialize for PublisherQoSConfList {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: serde::Serializer,
-    {
-        PublisherQoSConfList::serialize(self, serializer)
-    }
-}
 
 impl From<PublisherQoSConfList> for KeBoxTree<PublisherQoSConfig> {
     fn from(value: PublisherQoSConfList) -> KeBoxTree<PublisherQoSConfig> {
         let mut tree = KeBoxTree::new();
         for conf in value.0 {
             for key_expr in conf.key_exprs {
-                // key_expr unicity is checked at deserialization
+                // NOTE: we don't check key_expr unicity
                 tree.insert(&key_expr, conf.config.clone());
             }
         }
