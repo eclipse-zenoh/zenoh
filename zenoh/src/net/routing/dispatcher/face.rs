959fa1804b85654e08b26557d04784dbce6dd787
Mahmoud Mazouz <hello@fuzzypixelz.com>
2025-02-21 16:00:16 +0100

    Optimize `FaceState::update_interceptors_caches` (#1789)
    This function performs keyexpr allocation even if `InterceptorsChain` is `None`. Moreover, `InterceptorsChain` can apparently be `Some(..)` even if it's empty.

--- zenoh/src/net/routing/dispatcher/face.rs
+++ zenoh/src/net/routing/dispatcher/face.rs
@@ -15,6 +15,7 @@ use std::{
     any::Any,
     collections::HashMap,
     fmt,
+    ops::Not,
     sync::{Arc, Weak},
     time::Duration,
 };
@@ -144,8 +145,12 @@ impl FaceState {
     }
 
     pub(crate) fn update_interceptors_caches(&self, res: &mut Arc<Resource>) {
-        if let Ok(expr) = KeyExpr::try_from(res.expr().to_string()) {
-            if let Some(interceptor) = self.in_interceptors.as_ref() {
+        if let Some(interceptor) = self
+            .in_interceptors
+            .as_ref()
+            .and_then(|is| is.is_empty().not().then_some(is))
+        {
+            if let Ok(expr) = KeyExpr::try_from(res.expr().to_string()) {
                 let cache = interceptor.compute_keyexpr_cache(&expr);
                 get_mut_unchecked(
                     get_mut_unchecked(res)
@@ -155,7 +160,15 @@ impl FaceState {
                 )
                 .in_interceptor_cache = cache;
             }
-            if let Some(mux) = self.primitives.as_any().downcast_ref::<Mux>() {
+        }
+
+        if let Some(mux) = self
+            .primitives
+            .as_any()
+            .downcast_ref::<Mux>()
+            .and_then(|mux| mux.interceptor.is_empty().not().then_some(mux))
+        {
+            if let Ok(expr) = KeyExpr::try_from(res.expr().to_string()) {
                 let cache = mux.interceptor.compute_keyexpr_cache(&expr);
                 get_mut_unchecked(
                     get_mut_unchecked(res)
@@ -165,7 +178,14 @@ impl FaceState {
                 )
                 .e_interceptor_cache = cache;
             }
-            if let Some(mux) = self.primitives.as_any().downcast_ref::<McastMux>() {
+        }
+        if let Some(mux) = self
+            .primitives
+            .as_any()
+            .downcast_ref::<McastMux>()
+            .and_then(|mux| mux.interceptor.is_empty().not().then_some(mux))
+        {
+            if let Ok(expr) = KeyExpr::try_from(res.expr().to_string()) {
                 let cache = mux.interceptor.compute_keyexpr_cache(&expr);
                 get_mut_unchecked(
                     get_mut_unchecked(res)
