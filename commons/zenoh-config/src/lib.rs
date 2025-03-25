3ea794a766c0f720a0858b20717ac8040a92c0a0
DenisBiryukov91 <155981813+DenisBiryukov91@users.noreply.github.com>
2025-03-25 18:37:46 +0100

    Add qos overwrite interceptor (#1825)
    * add qos overwrite interceptor
    
    * simplify example config;
    add config validation tests;
    set flows to [egress, ingress ] if not specified;
    
    * check all message types
    
    * Update DEFAULT_CONFIG.json5
    
    Co-authored-by: Oussama Teffahi <70609372+oteffahi@users.noreply.github.com>
    
    * Update DEFAULT_CONFIG.json5
    
    Co-authored-by: Oussama Teffahi <70609372+oteffahi@users.noreply.github.com>
    
    * check for empty interfaces list
    
    * Reorganize conf
    
    * Update tracing
    
    * Remove key_expr from cache
    
    * Simplify code
    
    ---------
    
    Co-authored-by: Oussama Teffahi <70609372+oteffahi@users.noreply.github.com>
    Co-authored-by: OlivierHecart <olivier.hecart@adlinktech.com>

--- commons/zenoh-config/src/lib.rs
+++ commons/zenoh-config/src/lib.rs
@@ -32,7 +32,7 @@ use std::{
 };
 
 use include::recursive_include;
-use qos::PublisherQoSConfList;
+use qos::{PublisherQoSConfList, QosOverwriteItemConf};
 use secrecy::{CloneableSecret, DebugSecret, Secret, SerializableSecret, Zeroize};
 use serde::{Deserialize, Serialize};
 use serde_json::{Map, Value};
@@ -413,6 +413,8 @@ validated_struct::validator! {
         QoSConfig {
             /// A list of QoS configurations for PUT and DELETE messages by key expressions
             publication: PublisherQoSConfList,
+            /// Configuration of the qos overwrite interceptor rules
+            network: Vec<QosOverwriteItemConf>,
         },
 
         pub transport: #[derive(Default)]
