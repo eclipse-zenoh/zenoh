3f88c46197a10165b87d1fa41318e19b5e258a4a
yellowhatter <bannov.dy@gmail.com>
2025-04-01 10:06:45 +0300

    Fix memory leak in SHM segment cleanup by deregistering segment cleanup ids when necessary

--- commons/zenoh-shm/src/posix_shm/segment.rs
+++ commons/zenoh-shm/src/posix_shm/segment.rs
@@ -30,6 +30,17 @@ where
     shmem: shm::Segment<ID>,
 }
 
+impl<ID> Drop for Segment<ID>
+where
+    rand::distributions::Standard: rand::distributions::Distribution<ID>,
+    ID: shm::SegmentID,
+{
+    fn drop(&mut self) {
+        // Unregister unnecessary cleanup routine as Segment will be unlinked here
+        CLEANUP.read().unregister_cleanup(self.id());
+    }
+}
+
 impl<ID> Debug for Segment<ID>
 where
     ID: Debug,
@@ -54,16 +65,14 @@ where
             // Generate random id
             let id: ID = rand::thread_rng().gen();
 
-            // Register cleanup routine to make sure Segment will be unlinked on exit
-            CLEANUP.read().register_cleanup(Box::new(move || {
-                shm::Segment::ensure_not_persistent(id);
-            }));
-
             // Try to create a new segment identified by prefix and generated id.
             // If creation fails because segment already exists for this id,
             // the creation attempt will be repeated with another id
             match shm::Segment::create(id, len) {
                 Ok(shmem) => {
+                    // Register cleanup routine to make sure Segment will be unlinked on exit
+                    CLEANUP.read().register_cleanup(id);
+
                     tracing::debug!("Created SHM segment, len: {len}, id: {id}");
                     return Ok(Segment { shmem });
                 }
@@ -88,6 +97,9 @@ where
                 bail!("Unable to create POSIX shm segment: {}", e);
             }
         };
+
+        // Register cleanup routine to make sure Segment will be unlinked on exit
+        CLEANUP.read().register_cleanup(id);
 
         tracing::debug!("Opened SHM segment, id: {id}");
 
