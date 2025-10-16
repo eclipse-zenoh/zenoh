59114ec72455df29805004d3d4c1ccf28a1ee747
yellowhatter <bannov.dy@gmail.com>
2025-10-16 16:55:36 +0300

    add more safety comments

--- commons/zenoh-shm/src/shm/unix.rs
+++ commons/zenoh-shm/src/shm/unix.rs
@@ -269,6 +269,7 @@ impl<ID: SegmentID> SegmentImpl<ID> {
             flags,
             fd.as_raw_fd()
         );
+        // SAFETY: this is safe as we are not using any unsafe flags and FD is correct
         let ptr = unsafe { mmap(None, len, prot, flags, fd, 0) }?;
 
         tracing::trace!("mlock(addr={:?}, length={})", ptr, len,);
