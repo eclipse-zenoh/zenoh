c804457a6f9c5a26444a96c3664cca53f17a33bf
DenisBiryukov91 <155981813+DenisBiryukov91@users.noreply.github.com>
2025-10-17 10:05:18 +0200

    fix queryable info update for case of connection loss (#2205)

--- zenoh/tests/matching.rs
+++ zenoh/tests/matching.rs
@@ -114,7 +114,6 @@ fn is_locality_compatible(locality: Locality, same_session: bool) -> bool {
 async fn zenoh_querier_matching_status_inner(querier_locality: Locality, test_type: TestType) {
     println!("Querier origin :{querier_locality:?}, test type: {test_type:?}");
     let same_session = test_type == TestType::SameSession;
-    zenoh_util::init_log_from_env_or("error");
     let key_expr = match querier_locality {
         Locality::SessionLocal => "zenoh_querier_matching_status_local_test",
         Locality::Remote => "zenoh_querier_matching_status_remote_test",
@@ -253,7 +252,6 @@ async fn zenoh_querier_matching_status_inner(querier_locality: Locality, test_ty
 
 async fn zenoh_publisher_matching_status_inner(publisher_locality: Locality, same_session: bool) {
     println!("Publisher origin: {publisher_locality:?}, same session: {same_session}");
-    zenoh_util::init_log_from_env_or("error");
     let key_expr = match publisher_locality {
         Locality::SessionLocal => "zenoh_publisher_matching_status_local_test",
         Locality::Remote => "zenoh_publisher_matching_status_remote_test",
@@ -357,4 +355,82 @@ async fn zenoh_matching_listener_drop_deadlock() {
 
     drop(querier);
     drop(matching_listener); // Should not deadlock
+}
+
+async fn zenoh_querier_matching_status_session_drop_inner(test_type: TestType) {
+    println!("Querier session drop test type: {test_type:?}");
+    let key_expr = "zenoh_querier_matching_status_session_drop_test";
+
+    let mut router = None;
+    let (session1, session2) = match test_type {
+        TestType::ClientPeer => {
+            create_session_pair("tcp/127.0.0.1:18003", (WhatAmI::Client, WhatAmI::Peer)).await
+        }
+        TestType::PeerClient => {
+            create_session_pair("tcp/127.0.0.1:18003", (WhatAmI::Peer, WhatAmI::Client)).await
+        }
+        TestType::PeerPeer => {
+            create_session_pair("tcp/127.0.0.1:18003", (WhatAmI::Peer, WhatAmI::Peer)).await
+        }
+        TestType::ClientRouterClient => {
+            let mut c = zenoh::Config::default();
+            c.set_mode(Some(WhatAmI::Router)).unwrap();
+            c.listen
+                .set_endpoints(ModeDependentValue::Unique(vec!["tcp/127.0.0.1:18003"
+                    .parse()
+                    .unwrap()]))
+                .unwrap();
+            router = Some(ztimeout!(zenoh::open(c)).unwrap());
+            create_session_pair("tcp/127.0.0.1:18003", (WhatAmI::Client, WhatAmI::Client)).await
+        }
+        _ => unreachable!(),
+    };
+
+    let querier1 = ztimeout!(session1
+        .declare_querier(format!("{key_expr}/value"))
+        .target(zenoh::query::QueryTarget::BestMatching))
+    .unwrap();
+
+    let matching_listener1 = ztimeout!(querier1.matching_listener()).unwrap();
+
+    assert!(!ztimeout!(querier1.matching_status()).unwrap().matching());
+
+    assert_eq!(get_matching_listener_status(&matching_listener1), None);
+
+    let _qbl1 = ztimeout!(session2
+        .declare_queryable(format!("{key_expr}/value"))
+        .complete(false))
+    .unwrap();
+
+    assert_eq!(
+        get_matching_listener_status(&matching_listener1),
+        Some(true)
+    );
+    assert!(ztimeout!(querier1.matching_status()).unwrap().matching());
+
+    if let Some(r) = router.take() {
+        ztimeout!(r.close()).unwrap();
+    } else {
+        ztimeout!(session2.close()).unwrap();
+    }
+    assert_eq!(
+        get_matching_listener_status(&matching_listener1),
+        Some(false)
+    );
+    assert!(!ztimeout!(querier1.matching_status()).unwrap().matching());
+}
+
+#[tokio::test(flavor = "multi_thread", worker_threads = 4)]
+async fn zenoh_querier_matching_status_session_drop() -> ZResult<()> {
+    zenoh_util::init_log_from_env_or("error");
+    let test_types = [
+        TestType::ClientPeer,
+        TestType::PeerClient,
+        TestType::PeerPeer,
+        TestType::ClientRouterClient,
+    ];
+    for tt in test_types {
+        zenoh_querier_matching_status_session_drop_inner(tt).await;
+    }
+    Ok(())
 }
