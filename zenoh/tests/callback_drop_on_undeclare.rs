d0b526ae5e930213e35b208086d028559ec8ea40
ChenYing Kuo <evshary@gmail.com>
2026-03-27 17:24:36 +0800

    Fix the clippy issue.
    Signed-off-by: ChenYing Kuo <evshary@gmail.com>

--- zenoh/tests/callback_drop_on_undeclare.rs
+++ zenoh/tests/callback_drop_on_undeclare.rs
@@ -12,7 +12,6 @@
 //   ZettaScale Zenoh Team, <zenoh@zettascale.tech>
 //
 #![cfg(any(feature = "unstable", feature = "internal"))]
-#[allow(dead_code)]
 #[path = "common/mod.rs"]
 mod common;
 use core::time::Duration;
