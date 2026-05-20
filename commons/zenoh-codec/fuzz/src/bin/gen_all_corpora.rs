f25edcbaa98550bacaa3527eb0fffda246087a85
ChenYing Kuo <evshary@gmail.com>
2026-05-20 15:27:47 +0800

    style(fuzz): tidy aggregate corpus helper
    Signed-off-by: ChenYing Kuo <evshary@gmail.com>

--- commons/zenoh-codec/fuzz/src/bin/gen_all_corpora.rs
+++ commons/zenoh-codec/fuzz/src/bin/gen_all_corpora.rs
@@ -1,8 +1,7 @@
 use zenoh_codec_fuzz::write_all_seed_corpora;
 
 fn main() {
-    let written =
-        write_all_seed_corpora().expect("all corpus generation should succeed");
+    let written = write_all_seed_corpora().expect("all corpus generation should succeed");
     for path in written {
         println!("{}", path.display());
     }
